457accc50c5d6c37b5a29ca325faade1ec9c1f62
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-05-06 13:22:01 +0000

    restored clear button to searchbar.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@145 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/searchbar.php
+++ include/searchbar.php
@@ -143,6 +143,9 @@ function ResetTicks() {<?=$function?>}
 				  </div>
 				  <div class="tick"><label><input type="radio" name="display" value="list" <? if (getval("display","")=="list") { ?>checked="checked"<? } ?> />&nbsp;<?=$lang["list"]?></label></div>
 
+				<div class="SearchItem">
+
+				<input name="Clear" type="button" value="&nbsp;&nbsp;<?=$lang["clearbutton"]?>&nbsp;&nbsp;" onClick="document.getElementById('ssearchbox').value='';document.getElementById('basiccountry').value='';document.getElementById('basicyear').value='';document.getElementById('basicmonth').value='';document.getElementById('basicday').value='';ResetTicks();"/><input name="Submit" type="submit" value="&nbsp;&nbsp;<?=$lang["searchbutton"]?>&nbsp;&nbsp;" /></div>
   </form>
 	
   <p><br /><a href="<?=$baseurl?>/search_advanced.php">&gt; <?=$lang["gotoadvancedsearch"]?></a></p>
