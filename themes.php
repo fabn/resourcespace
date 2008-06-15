483e3bd2c398b21acc8236321bef21240d1952a1
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-06-15 21:01:35 +0000

    Altered smart themes so field permissions are used to determine if the theme should be displayed.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@203 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- themes.php
+++ themes.php
@@ -60,25 +60,28 @@ if (!$manage)
 	$headers=get_smart_theme_headers();
 	for ($n=0;$n<count($headers);$n++)
 		{
-		?>
-		<div class="ThemeBox">
-		<h1 class="NavUnderline" style="margin-bottom:10px;padding-bottom:2px;margin-top:15px;">
-		<?=i18n_get_translated($headers[$n]["smart_theme_name"])?></h1>
-		<ul>
-		<?
-		$themes=get_smart_themes($headers[$n]["ref"]);
-		for ($m=0;$m<count($themes);$m++)
+		if (checkperm("f*") || checkperm("f" . $headers[$n]["ref"]))
 			{
-			$s=$headers[$n]["name"] . ":" . $themes[$m];
 			?>
-			<li><a href="search.php?search=<?=urlencode($s)?>"><?=htmlspecialchars(i18n_get_translated($themes[$m]))?></a>
-			</li>
+			<div class="ThemeBox">
+			<h1 class="NavUnderline" style="margin-bottom:10px;padding-bottom:2px;margin-top:15px;">
+			<?=i18n_get_translated($headers[$n]["smart_theme_name"])?></h1>
+			<ul>
+			<?
+			$themes=get_smart_themes($headers[$n]["ref"]);
+			for ($m=0;$m<count($themes);$m++)
+				{
+				$s=$headers[$n]["name"] . ":" . $themes[$m];
+				?>
+				<li><a href="search.php?search=<?=urlencode($s)?>"><?=htmlspecialchars(i18n_get_translated($themes[$m]))?></a>
+				</li>
+				<?
+				}
+			?>
+			</ul>
+			</div>
 			<?
 			}
-		?>
-		</ul>
-		</div>
-		<?
 		}
 	}
 ?>
