15504e6b0a7e60e68c05743138d5720d4c89f64f
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-07-27 22:26:21 +0000

    Replaced 'add' with '+' on multi-user selector. Some languages could not fit the equiv. text on the button.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@304 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/user_select.php
+++ include/user_select.php
@@ -5,7 +5,7 @@
 ?>
 <table cellpadding="0" cellspacing="0" width="300">
 <tr><td><input type="text" class="stdwidth" style="width:245px;" value="<?=$lang["starttypingusername"]?>" id="autocomplete" name="autocomplete_parameter" onClick="this.value='';" /></td>
-<td><input type=button value="<?=$lang["add"]?>" style="width:48px;" onClick="addUser();" /></td></tr>
+<td><input type=button value="+" style="width:48px;" onClick="addUser();" /></td></tr>
 <tr><td colspan="2" align="left"><textarea rows=6 class="stdwidth" style="width:300px;" name="users" id="users"><? if (isset($userstring)) {echo $userstring;} ?></textarea></td></tr>
 </table>
 <div class="clearer"> </div>
