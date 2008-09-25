98b40f9f2ebb3e7a4557a2e321290dd3543041bb
stefan <stefan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-09-25 09:34:46 +0000

    Fixed recognition of jpeg files (was missing the extension with e in it)
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@396 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/image_processing.php
+++ include/image_processing.php
@@ -377,7 +377,7 @@ function create_previews_using_im($ref,$thumbonly=false,$extension="jpg",$previe
 			$tw=$ps[$n]["width"];$th=$ps[$n]["height"];
 			$id=$ps[$n]["id"];
 
-			if ((!$highestsize && ($extension != "jpg")) || ($sw>$tw) || ($sh>$th) || ($id == "pre") || ($id=="thm") || ($id=="col"))
+			if ((!$highestsize && !eregi("jp[e]?g", $extension)) || ($sw>$tw) || ($sh>$th) || ($id == "pre") || ($id=="thm") || ($id=="col"))
 				{
 				if (($sw<$tw) && ($sh<$th))
 					{
