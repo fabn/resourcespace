1787ef60e86b6e2bb16bb066e3779734e5d06431
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-09-26 14:58:22 +0000

    Moved file extension to the end for the list view.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@426 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- search.php
+++ search.php
@@ -329,8 +329,10 @@ if (true) #search condition
 			<!--List Item-->
 			<tr>
 			<td nowrap><div class="ListTitle"><a <? if ($infobox) { ?>onMouseOver="InfoBoxSetResource(<?=$ref?>);" onMouseOut="InfoBoxSetResource(0);"<? } ?> href="<?=$url?>"><?=highlightkeywords(tidy_trim($result[$n]["title"],45) . 
-			($show_extension_in_search?" [" . strtoupper($result[$n]["file_extension"]) . "]":"") .
-			((strlen(trim($result[$n]["country"]))>1)?(", " . tidy_trim(TidyList(i18n_get_translated($result[$n]["country"])),25)):""),$search) ?></a></div></td>
+			
+			((strlen(trim($result[$n]["country"]))>1)?(", " . tidy_trim(TidyList(i18n_get_translated($result[$n]["country"])),25)):"") .
+			($show_extension_in_search?" [" . strtoupper($result[$n]["file_extension"]) . "]":"")
+			,$search) ?></a></div></td>
 			<td><? if ($result[$n]["rating"]>0) { ?><div class="IconStar"> </div><? } else { ?>&nbsp;<? } ?></td>
 			<td><?=$result[$n]["ref"]?></td>
 			<td><? if (array_key_exists($result[$n]["resource_type"],$rtypes)) { ?><?=i18n_get_translated($rtypes[$result[$n]["resource_type"]])?><? } ?></td>
