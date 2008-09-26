aaafac3f95dcbd8f54380f8e33fc44d3c3657ce8
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-09-26 06:37:50 +0000

    Bug fix when using preview.php - the 'back to view' link doesn't pass the 'k' value so it didn't work for external users.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@418 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- preview.php
+++ preview.php
@@ -50,7 +50,7 @@ if (!file_exists(get_resource_path($ref,"scr",false,$ext,-1,$nextpage))) {$nextp
 include "include/header.php";
 ?>
 
-<p style="margin:7px 0 7px 0;padding:0;"><a href="view.php?ref=<?=$ref?>&search=<?=urlencode($search)?>&offset=<?=$offset?>&order_by=<?=$order_by?>&archive=<?=$archive?>">&lt; <?=$lang["backtoview"]?></a>
+<p style="margin:7px 0 7px 0;padding:0;"><a href="view.php?ref=<?=$ref?>&search=<?=urlencode($search)?>&offset=<?=$offset?>&order_by=<?=$order_by?>&archive=<?=$archive?>&k=<?=$k?>">&lt; <?=$lang["backtoview"]?></a>
 <? if ($k=="") { ?>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="collections.php?add=<?=$ref?>&nc=<?=time()?>&search=<?=urlencode($search)?>" target="collections"> <?=$lang["addtocollection"]?></a>
