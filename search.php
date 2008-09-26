a9384eecfb3ec5fc1e7fac674387f4a112f7dbe4
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-09-26 14:56:12 +0000

    Shortened country text on search results to make room for new full screen preview icon.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@425 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- search.php
+++ search.php
@@ -282,7 +282,7 @@ if (true) #search condition
 
 <? } ?> <!-- END HOOK Rendertitlethumb -->			
 			
-			<div class="ResourcePanelCountry"><? if (!$allow_reorder) { # Do not display the country if reordering (to create more room) ?><?=highlightkeywords(tidy_trim(TidyList(i18n_get_translated($result[$n]["country"])),14),$search)?><? } ?>&nbsp;</div>				
+			<div class="ResourcePanelCountry"><? if (!$allow_reorder) { # Do not display the country if reordering (to create more room) ?><?=highlightkeywords(tidy_trim(TidyList(i18n_get_translated($result[$n]["country"])),10),$search)?><? } ?>&nbsp;</div>				
 			<span class="IconPreview"><a href="preview.php?from=search&ref=<?=$ref?>&ext=<?=$result[$n]["preview_extension"]?>&search=<?=urlencode($search)?>&offset=<?=$offset?>&order_by=<?=$order_by?>&archive=<?=$archive?>" title="<?=$lang["fullscreenpreview"]?>"><img src="gfx/interface/sp.gif" alt="" width="22" height="12" /></a></span>
 			<? if (!checkperm("b")) { ?><span class="IconCollect"><a href="collections.php?add=<?=$ref?>&nc=<?=time()?>&search=<?=urlencode($search)?>" target="collections" <? if (!$infobox) { ?>title="<?=$lang["addtocurrentcollection"]?>"<? } ?>><img src="gfx/interface/sp.gif" alt="" width="22" height="12" /></a></span><? } ?>
 			<? if ($allow_share) { ?><span class="IconEmail"><a href="resource_email.php?ref=<?=$ref?>" <? if (!$infobox) { ?>title="<?=$lang["emailresource"]?>"<? } ?>><img src="gfx/interface/sp.gif" alt="" width="16" height="12" /></a></span><? } ?>
