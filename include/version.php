aef3c4f6bc2ceb2daec970c38ba32aa36984b310
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-10-07 14:26:11 +0000

    Moved css_reload_key into version.php otherwise it would be overwritten by the local copied config.php value.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@438 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/version.php
+++ include/version.php
@@ -1,4 +1,9 @@
 <?
 $productname="ResourceSpace"; # Product name. Do not change.
 $productversion="1.6dev";
+
+# External items reload key
+# Increment the below value to force a reload of the CSS and other included items.
+# This is intended for developer use when altering CSS.
+$css_reload_key=1;
 ?>
