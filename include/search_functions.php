6dabfc3b3c5b4b9fdf94db21e03a6c3596456a59
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-11-10 13:58:35 +0000

    Fixed issue with user contributions (archive status is still important even if other filters are ignored).
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@556 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_functions.php
+++ include/search_functions.php
@@ -177,7 +177,7 @@ function do_search($search,$restypes="",$order_by="relevance",$archive=0,$fetchr
 	if (substr($search,0,14)=="!contributions") 
 		{
 		global $userref;
-		if ($userref==str_replace("!contributions","",$search)) {$filter="1=1";$custperm="";} # Disable permissions when viewing your own contributions
+		if ($userref==str_replace("!contributions","",$search)) {$filter="archive='$archive'";$custperm="";} # Disable permissions when viewing your own contributions - only restriction is the archive status
 		return sql_query("select r.*,r.hit_count score from resource r $custperm where created_by='" . str_replace("!contributions","",$search) . "' and $filter group by r.ref order by $order_by",false,$fetchrows);
 		}
 	
