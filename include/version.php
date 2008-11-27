13b9f58bd6f472590989435f4c1748dc0284b026
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2008-11-27 10:27:23 +0000

    SWFUpload upgraded to 2.2.0 Beta 3 following some reported issues.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@592 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/version.php
+++ include/version.php
@@ -5,5 +5,5 @@ $productversion="1.7dev";
 # External items reload key
 # Increment the below value to force a reload of the CSS and other included items.
 # This is intended for developer use when altering CSS.
-$css_reload_key=5;
+$css_reload_key=6;
 ?>
