f53bfa0297f5bd1ad8005782ac8493326019734f
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-01-22 13:37:01 +0000

    Bug fix - altered space replacement code for user selection - it was replacing too much.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@626 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/user_select.php
+++ include/user_select.php
@@ -7,7 +7,7 @@
 <tr><td><input type="text" class="stdwidth" style="width:245px;" value="<?php echo $lang["starttypingusername"]?>" id="autocomplete" name="autocomplete_parameter" onClick="this.value='';" /></td>
 <td><input type=button value="+" style="width:48px;" onClick="addUser();" /></td></tr>
 <tr><td colspan="2" align="left"><textarea rows=6 class="stdwidth" style="width:300px;" name="users" id="users"
-onChange="this.value=this.value.replace(/[^,] /g,', ');"><?php if (isset($userstring)) {echo $userstring;} ?></textarea></td></tr>
+onChange="this.value=this.value.replace(/[^,] /g,function replacespaces(str) {return str.substring(0,1) + ', ';});"><?php if (isset($userstring)) {echo $userstring;} ?></textarea></td></tr>
 </table>
 
 
