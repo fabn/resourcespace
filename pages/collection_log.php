b02231c32ac3b53397c8661a344af82cbd7f8a20
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-02-17 03:10:15 +0000

    added collection_log.php
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@678 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ pages/collection_log.php
@@ -0,0 +1,44 @@
+<?php
+include "../include/db.php";
+include "../include/authenticate.php";
+include "../include/general.php";
+include "../include/collections_functions.php";
+
+$ref=getvalescaped("ref","");
+
+include "../include/header.php";
+?>
+<div class="BasicsBox">
+<h1><?php echo $lang["collectionlog"]?></h1>
+</div>
+
+<div class="Listview">
+<table border="0" cellspacing="0" cellpadding="0" class="ListviewStyle">
+<!--Title row-->	
+<tr class="ListviewTitleStyle">
+<td><?php echo $lang["date"]?></td>
+<td><?php echo $lang["user"]?></td>
+<td><?php echo $lang["action"]?></td>
+<td><?php echo $lang["resourcetitle"]?></td>
+</tr>
+
+<?php
+$log=get_collection_log($ref);
+for ($n=0;$n<count($log);$n++)
+	{
+	?>
+	<!--List Item-->
+	<tr>
+	<td><?php echo $log[$n]["date"]?></td>
+	<td><?php echo $log[$n]["username"]?> (<?php echo $log[$n]["fullname"]?>)</td>
+	<td><?php echo $lang["collectionlog-" . $log[$n]["type"]]?></td>
+	<td><?php echo i18n_get_translated($log[$n]["title"])?></td>
+	</tr>
+	<?php
+	}
+?>
+</table>
+</div>
+<?php
+include "../include/footer.php";
+?>
