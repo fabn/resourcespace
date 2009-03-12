da1d8ba6d0d6dff5eb3ef32e5147e0e8719da2a6
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-03-12 17:12:59 +0000

    added player_flv_maxi.swf and volume/fullscreen options to video playback
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@751 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/flv_play.php
+++ pages/flv_play.php
@@ -23,7 +23,9 @@ $colour="505050";
 if ($theme=="greyblu") {$colour="446693";}
 
 ?>
-<object type="application/x-shockwave-flash" data="../lib/flashplayer/player_flv.swf" width="<?php echo $ffmpeg_preview_max_width?>" height="<?php echo $ffmpeg_preview_max_height?>" class="Picture">
-     <param name="movie" value="../lib/flashplayer/player_flv.swf" />
-     <param name="FlashVars" value="flv=<?php echo $flashpath?>&amp;width=<?php echo $ffmpeg_preview_max_width?>&amp;height=<?php echo $ffmpeg_preview_max_height?>&amp;margin=0&amp;buffer=10&amp;autoload=0&amp;showstop=1&amp;playercolor=<?php echo $colour?>" />
+<object type="application/x-shockwave-flash" data="../lib/flashplayer/player_flv_maxi.swf" width="<?php echo $ffmpeg_preview_max_width?>" height="<?php echo $ffmpeg_preview_max_height?>" class="Picture">
+                    <param name="allowFullScreen" value="true" />
+
+     <param name="movie" value="../lib/flashplayer/player_flv_maxi.swf" />
+     <param name="FlashVars" value="flv=<?php echo $flashpath?>&amp;width=<?php echo $ffmpeg_preview_max_width?>&amp;height=<?php echo $ffmpeg_preview_max_height?>&amp;margin=0&amp;buffer=10&amp;showvolume=1&amp;showtime=1&amp;autoload=0&amp;showfullscreen=1&amp;showstop=1&amp;playercolor=<?php echo $colour?>" />
 </object>
