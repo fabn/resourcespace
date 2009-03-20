4e38317b1aba7450a08b1aa3494bd429dd12ac0d
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-03-20 11:50:44 +0000

    removed prefix in the new imagemagick code after RAW processing is done. Error on for CR2s before this.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@777 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/image_processing.php
+++ include/image_processing.php
@@ -436,7 +436,7 @@ function create_previews_using_im($ref,$thumbonly=false,$extension="jpg",$previe
 			if (!file_exists($command)) {$command=$imagemagick_path . "\convert.exe";}
 			if (!file_exists($command)) {exit("Could not find ImageMagick 'convert' utility.'");}	
 			
-			$command .= ' '. escapeshellarg($prefix.$file) .'[0]  -flatten -quality ' . $imagemagick_quality;
+			$command .= ' '. escapeshellarg($file) .'[0]  -flatten -quality ' . $imagemagick_quality;
 			
 			# fetch target width and height
 			$tw=$ps[$n]["width"];$th=$ps[$n]["height"];
