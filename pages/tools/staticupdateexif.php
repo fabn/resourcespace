81ca5e04e23a656561180ea009d8c6a42e4b84cc
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-03-29 18:49:29 +0000

    fix for staticupdateexif.php
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@808 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/staticupdateexif.php
+++ pages/tools/staticupdateexif.php
@@ -8,11 +8,11 @@ set_time_limit(60*60*40);
 
 echo "Updating EXIF/IPTC...";
 
-$rd=sql_array("select ref value from resource where has_image=1 and resource_type=1");
+$rd=sql_query("select ref,file_extension from resource where has_image=1 and resource_type=1");
 for ($n=0;$n<count($rd);$n++)
 	{
-	echo "." . $rd[$n];
-	extract_exif_comment($rd[$n]);
+	echo "." . $rd[$n]['ref'];
+	extract_exif_comment($rd[$n]['ref'],$rd[$n]['file_extension']);
 	}
 echo "...done.";
 
