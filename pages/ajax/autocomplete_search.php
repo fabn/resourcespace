f6852c8361bda2712da053480dfcd9577a6518ea
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-04-01 09:47:52 +0000

    Made a small tweak to search autocompletion in the hope that it will fix the 'white-out' issue with IE6.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@817 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/ajax/autocomplete_search.php
+++ pages/ajax/autocomplete_search.php
@@ -14,10 +14,12 @@ $last=$s[count($s)-1];
 # Merge the words back together so existing words can be added to the results.
 array_pop($s);$otherwords=join(" ",$s);
 
+?>
+<ul>
+<?php
 if (strlen($last)>=2) # Activate when last entered keyword >=3 chars long
 	{
 	?>
-	<ul>
 	<?php
 	$keywords=get_suggested_keywords($last);
 	for ($n=0;$n<count($keywords);$n++)
@@ -27,7 +29,7 @@ if (strlen($last)>=2) # Activate when last entered keyword >=3 chars long
 		<?php
 		}
 	?>
-	</ul>
 	<?php
 	}
 ?>
+</ul>
