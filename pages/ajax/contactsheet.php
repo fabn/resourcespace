626c29d7be3ef157da924e701e07ef3c51f1848d
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-04-02 00:01:29 +0000

    fix for contact sheet generated as resource. redirect to view.php was broken after moving contactsheet.php to ajax folder.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@818 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/ajax/contactsheet.php
+++ pages/ajax/contactsheet.php
@@ -214,7 +214,7 @@ relate_to_collection($newresource,$collection);
 	
 	#Create thumbnails and redirect browser to the new contact sheet resource
 	create_previews($newresource,true,"pdf");
-	redirect("../pages/view.php?ref=" .$newresource);
+	redirect("pages/view.php?ref=" .$newresource);
 	}
 
 else
