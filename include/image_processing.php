36fc476e4635aaff360c4b375884e7f58c5a65b1
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-05-15 00:50:29 +0000

    not exactly sure why, but spaces in the uploaded file name have been replaced with underscores in the filename field since revision 781
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@921 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/image_processing.php
+++ include/image_processing.php
@@ -11,7 +11,7 @@ function upload_file($ref)
 	# Work out which file has been posted (switch is necessary for SWFUpload)
 	if (isset($_FILES['userfile'])) {$processfile=$_FILES['userfile'];} else {$processfile=$_FILES['Filedata'];}
 	
-    $filename=str_replace(" ","_",$processfile['name']);
+    $filename=$processfile['name'];
     
     # Work out extension
     global $exiftool_path;
