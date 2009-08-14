510fa8ab9f9375f95818b5aed0a4a41345b3ec66
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-08-14 20:37:46 +0000

    added hook replacecollectionsresearchlinks for minimized view
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1252 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collections.php
+++ pages/collections.php
@@ -495,7 +495,7 @@ if ($k!="")
 <div id="CollectionMinRightNav">
   <ul>
 <?php if ((!collection_is_research_request($usercollection)) || (!checkperm("r"))) { ?>
-    		<?php if (checkperm("s")) { ?><li><a href="collection_manage.php" target="main"><?php echo $lang["managemycollections"]?></a></li>
+    <?php if (checkperm("s")) { ?><li><a href="collection_manage.php" target="main"><?php echo $lang["managemycollections"]?></a></li>
     <?php if ($allow_share) { ?><li><a href="collection_share.php?ref=<?php echo $usercollection?>" target="main"><?php echo $lang["share"]?></a></li><?php } ?>
     
     <?php if (($userref==$cinfo["user"]) || (checkperm("h"))) {?><li><a target="main" href="collection_edit.php?ref=<?php echo $usercollection?>">&nbsp;<?php echo $allow_share?$lang["edit"]:$lang["editcollection"]?></a></li><?php } ?>
@@ -504,11 +504,13 @@ if ($k!="")
     
     <?php } ?>
     <?php } else {
-    $research=sql_value("select ref value from research_request where collection='$usercollection'",0);
-    ?>
+	if (!hook("replacecollectionsresearchlinks")){	
+    $research=sql_value("select ref value from research_request where collection='$usercollection'",0);	
+	?>
     <li><a href="team/team_research.php" target="main"><?php echo $lang["manageresearchrequests"]?></a></li>   
-    <li><a href="team/team_research_edit.php?ref=<?php echo $research?>" target="main"><?php echo $lang["editresearchrequests"]?></a></li>     
-    <?php } ?>
+    <li><a href="team/team_research_edit.php?ref=<?php echo $research?>" target="main"><?php echo $lang["editresearchrequests"]?></a></li>         
+    <?php } /* end hook replacecollectionsresearchlinks */ ?>	
+	<?php } ?>
     <?php 
     # If this collection is (fully) editable, then display an extra edit all link
     if ((count($result)>0) && checkperm("e" . $result[0]["archive"]) && allow_multi_edit($usercollection)) { ?>
