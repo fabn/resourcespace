410ff0182713c996002b0a7ec5c662dc910ffdbb
badams <badams@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-10-21 19:23:05 +0000

    Fix typo in create_previews
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1448 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/image_processing.php
+++ include/image_processing.php
@@ -539,7 +539,7 @@ function create_previews($ref,$thumbonly=false,$extension="jpg",$previewonly=fal
 					}
 				}
 			# flag database so a thumbnail appears on the site
-			if ($alernative==-1) # not for alternatives
+			if ($alternative==-1) # not for alternatives
 				{
 				sql_query("update resource set has_image=1,preview_extension='jpg',file_modified=now() where ref='$ref'");
 				}
