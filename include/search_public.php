75390eefa93a23584678f013b0defb50e10c486e
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-12-11 15:40:42 +0000

    fix for adding an appropriate number of columns in list display for collection results
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1581 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_public.php
+++ include/search_public.php
@@ -105,6 +105,13 @@ for ($n=0;$n<count($collections);$n++)
 		?>
 		<tr>
 		<td nowrap><div class="ListTitle"><a href="<?php echo $pub_url?>"><?php echo $lang["collection"] . ": " . highlightkeywords(tidy_trim(i18n_get_translated($collections[$n]["name"]),45),$search)?></a></div></td>
+		<?php 
+		if (!$use_resource_column_data){
+			for ($x=0;$x<count($ldf)-1;$x++){
+				?><td>&nbsp;</td><?php
+				}
+			}	
+		?>
 		<td>&nbsp;</td>
 		<td>&nbsp;</td>
 		<td><?php echo $lang["collection"] ?></td>
