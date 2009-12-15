fabn/resourcespace

commit 7d971102ebe2df22ca90b44e121b9206451d6b9a

added ability to set allowed extensions for particular resource types.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1147 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/general.php
@@ -1861,5 +1861,30 @@ $strName = substr($strName, 0, -strlen($ext));
 return $strName;
 }
 
+function verify_extension($filename,$allowed_extensions=""){
+	# Allowed extension?
+	$extension=explode(".",$filename);
+    if(count($extension)>1){
+    	$extension=trim(strtolower($extension[count($extension)-1]));
+		} else { return false;}
+		
+	if ($allowed_extensions!=""){
+		$allowed_extensions=explode(",",strtolower($allowed_extensions));
+		if (!in_array($extension,$allowed_extensions)){ return false;}
+	}
+	
+	
+	return true;
+}
+
+function get_allowed_extensions($ref){
+	$type = sql_value("select resource_type value from resource where ref=$ref","");
+	$allowed_extensions=sql_value("select allowed_extensions value from resource_type where ref=$type","");
+	return $allowed_extensions;
+}
+function get_allowed_extensions_by_type($resource_type){
+	$allowed_extensions=sql_value("select allowed_extensions value from resource_type where ref=$resource_type","");
+	return $allowed_extensions;
+}
 
 ?>

pages/upload_swf.php
@@ -6,8 +6,10 @@ include "../include/image_processing.php";
 include "../include/resource_functions.php";
 include "../include/collections_functions.php";
 $status="";
-
+$resource_type=getvalescaped("resource_type","");
 $collection_add=getvalescaped("collection_add","");
+
+$allowed_extensions=get_allowed_extensions_by_type($resource_type);
 
 # Create a new collection?
 if ($collection_add==-1)
@@ -25,19 +27,23 @@ if (array_key_exists("Filedata",$_FILES))
     if (getval("replace","")=="")
     	{
 		# New resource
-		$ref=copy_resource(0-$userref); # Copy from user template
 		
-		# Add to collection?
-		if ($collection_add!="")
-			{
-			add_resource_to_collection($ref,$collection_add);
-			}
+		if(verify_extension($_FILES['Filedata']['name'],$allowed_extensions)){
+		
+			$ref=copy_resource(0-$userref); # Copy from user template
+		
+			# Add to collection?
+			if ($collection_add!="")
+				{
+				add_resource_to_collection($ref,$collection_add);
+				}
 			
-		# Log this			
-		daily_stat("Resource upload",$ref);
-		resource_log($ref,"u",0);
+			# Log this			
+			daily_stat("Resource upload",$ref);
+			resource_log($ref,"u",0);
 	
-		$status=upload_file($ref);
+			$status=upload_file($ref);
+		}
 		
 		$thumb=get_resource_path($ref,true,"col",false);
 		if (file_exists($thumb))
@@ -179,7 +185,7 @@ window.onload =  function()
 	{
 
 	swfu = new SWFUpload({
-		upload_url : "<?php echo $baseurl?>/pages/upload_swf.php?replace=<?php echo getval("replace","")?>&collection_add=<?php echo $collection_add?>&user=<?php echo urlencode($_COOKIE["user"])?>",
+		upload_url : "<?php echo $baseurl?>/pages/upload_swf.php?replace=<?php echo getval("replace","")?>&collection_add=<?php echo $collection_add?>&user=<?php echo urlencode($_COOKIE["user"])?>&resource_type=<?php echo $resource_type?>",
 		flash_url : "<?php echo $baseurl?>/lib/swfupload/swfupload.swf",
 		
 
@@ -238,6 +244,7 @@ function debug()
 <h2>&nbsp;</h2>
 <h1><?php echo (getval("replace","")!="")?$lang["replaceresourcebatch"]:$lang["fileupload"]?></h1>
 <p><?php echo text("introtext")?></p>
+<?php if ($allowed_extensions!=""){?><p><?php echo $lang['allowedextensions'].": ".$allowed_extensions?></p><?php } ?>
 
 <br/>
 <?php if ($status!="") { ?><?php echo $status?><?php } ?>

languages/en.php
@@ -786,8 +786,8 @@ $lang["addremove"]="Add/Remove";
 $lang["all_users"]="all users";
 $lang["new_resource"]="new resource";
 
-
-
+$lang["invalidextension_mustbe"]="Invalid extension, must be";
+$lang["allowedextensions"]="Allowed Extensions";
 
 
 ?>

lib/swfupload/handlers.js
@@ -87,7 +87,7 @@ function uploadComplete(fileObj) {
 		} else {
 			var progress = new FileProgress(fileObj,  this.customSettings.upload_target);
 			progress.SetComplete();
-			progress.SetStatus("All images received.");
+			progress.SetStatus("Done.");
 			progress.ToggleCancel(false);
 		}
 	} catch (ex) { this.debug(ex); }

pages/create.php
@@ -24,11 +24,15 @@ include "../include/header.php";
 
 <form method=post>
 <input type=hidden name="archive" value="<?php echo $archive?>">
-
+<script type="text/javascript"> function hideAllExtensions(){
+	$$('div[class="extensions"]').each(function(elem){
+       elem.style.display='none';
+     });
+	}</script>
 <div class="Question">
 <label for="resourcetype"><?php echo $lang["resourcetype"]?></label>
 <div class="tickset">
-<div class="Inline"><select name="resource_type" class="shrtwidth">
+<div class="Inline"><select name="resource_type" class="shrtwidth" OnChange="var type=$(this).value;hideAllExtensions();$(type).style.display='block';">
 <?php
 $types=get_resource_types();
 for ($n=0;$n<count($types);$n++)
@@ -36,6 +40,15 @@ for ($n=0;$n<count($types);$n++)
 	?><option value="<?php echo $types[$n]["ref"]?>"><?php echo $types[$n]["name"]?></option><?php
 	}
 ?></select></div>
+<?php
+for ($n=0;$n<count($types);$n++){
+	$allowed_extensions=get_allowed_extensions_by_type($types[$n]["ref"]);	
+	?>
+	<div class="clearerleft"></div>
+	<div class="extensions" id="<?php echo $types[$n]["ref"]?>" style="display:none;">
+	<?php if ($allowed_extensions!=""){echo $lang['allowedextensions'].": ".$allowed_extensions;}?></div>
+<?php } ?>
+
 <div class="Inline"><input name="Submit" type="submit" value="&nbsp;&nbsp;<?php echo $lang["create"]?>&nbsp;&nbsp;" /></div>
 </div>
 <div class="clearerleft"> </div>

pages/edit.php
@@ -109,22 +109,22 @@ if (getval("submitted","")!="" && getval("resetform","")=="" && getval("copyfrom
 				if (getval("swf","")!="") // Test if in browser flash upload
 					{
 					# Save button pressed? Move to next step.
-					if (getval("save","")!="") {redirect("pages/upload_swf.php?collection_add=" . getval("collection_add",""));}
+					if (getval("save","")!="") {redirect("pages/upload_swf.php?collection_add=" . getval("collection_add","")."&resource_type=".$resource_type);}
 					}
 				elseif (getval("java","")!="") // Test if in browser java upload
 					{
 					# Save button pressed? Move to next step.
-					if (getval("save","")!="") {redirect("pages/upload_java.php?collection_add=" . getval("collection_add",""));}
+					if (getval("save","")!="") {redirect("pages/upload_java.php?collection_add=" . getval("collection_add","")."&resource_type=".$resource_type);}
 					}
 				elseif (getval("local","")!="") // Test if fetching resource from local upload folder.
 					{
 					# Save button pressed? Move to next step.
-					if (getval("save","")!="") {redirect("pages/team/team_batch_select.php?use_local=yes");}
+					if (getval("save","")!="") {redirect("pages/team/team_batch_select.php?use_local=yes&resource_type=".$resource_type);}
 					}
 				else
 					{
 					# Save button pressed? Move to next step.
-					if (getval("save","")!="") {redirect("pages/team/team_batch.php");}
+					if (getval("save","")!="") {redirect("pages/team/team_batch.php&resource_type=".$resource_type);}
 					}
 				}
 			}

pages/team/team_batch.php
@@ -4,12 +4,13 @@ include "../../include/authenticate.php"; if (!checkperm("c")) {exit ("Permissio
 include "../../include/general.php";
 
 include "../../include/header.php";
+$resource_type=getvalescaped('resource_type','');
 ?>
 <div class="BasicsBox">
 	<h1><?php echo $lang["specifyftpserver"]?></h1>
 	<p><?php echo text("introtext")?></p>
 
-	<form method="post" action="team_batch_select.php">
+	<form method="post" action="team_batch_select.php?resource_type=<?php echo $resource_type?>">
 
 		<div class="Question"><label><?php echo $lang["ftpserver"]?></label><input name="ftp_server" type="text" class="stdwidth" value="<?php echo $ftp_server?>"><div class="clearerleft"> </div></div>
 

pages/team/team_batch_select.php
@@ -5,6 +5,8 @@ include "../../include/general.php";
 include "../../include/collections_functions.php";
 
 $use_local = getvalescaped('use_local', '') !== '';
+$resource_type=getvalescaped('resource_type','');
+$allowed_extensions=get_allowed_extensions_by_type($resource_type);
 
 if ($use_local)
 	{
@@ -88,7 +90,8 @@ for ($n=0;$n<count($list);$n++)
 <div class="Question"><label><?php echo $lang["selectfiles"]?></label>
 <!--<div class="tickset">-->
 <select name="uploadfiles[]" multiple size=20>
-<?php for ($n=0;$n<count($files);$n++)
+<?php 
+for ($n=0;$n<count($files);$n++)
 	{
 	if ($use_local) {$fn=$files[$n];} else
 		{
@@ -102,6 +105,16 @@ for ($n=0;$n<count($list);$n++)
 	if (strpos($fn,".")===false) {$show=false;}
 	if ($fn=="pspbrwse.jbf") {$show=false;} # Ignore PSP browse files (often imported by mistake)
 	if ($fn==".DS_Store") {$show=false;} # Ignore .DS_Store file on the mac
+	
+	# omit disallowed extensions
+	if ($allowed_extensions!=""){
+	    $extension=explode(".",$fn);
+		if(count($extension)>1){
+    	$extension=trim(strtolower($extension[count($extension)-1]));
+		} 
+		if (!strstr($allowed_extensions,$extension)){$show=false;}
+	}
+	
 	/* if ($show) { ?><div class="tick"><input type="checkbox" name="uploadfiles[]" value="<?php echo $fn?>" checked /><?php echo $fn?></div><?php } ?>
 	*/
 	if ($show) { ?><option value="<?php echo $fn?>" selected><?php echo $fn?></option><?php } ?>

commit 20aba75fcf2d93c13d11d3e13af71634d1c6c666

Fixed 'replace resource batch' which was broken in r1147 with the introduction of allowed extensions.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1594 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/general.php
@@ -2026,7 +2026,7 @@ function get_allowed_extensions($ref){
 	return $allowed_extensions;
 }
 function get_allowed_extensions_by_type($resource_type){
-	$allowed_extensions=sql_value("select allowed_extensions value from resource_type where ref=$resource_type","");
+	$allowed_extensions=sql_value("select allowed_extensions value from resource_type where ref='$resource_type'","");
 	return $allowed_extensions;
 }
 

pages/upload_swf.php
@@ -10,7 +10,8 @@ $resource_type=getvalescaped("resource_type","");
 $collection_add=getvalescaped("collection_add","");
 $collectionname=getvalescaped("entercolname","");
 
-$allowed_extensions=get_allowed_extensions_by_type($resource_type);
+$allowed_extensions="";
+if ($resource_type!="") {$allowed_extensions=get_allowed_extensions_by_type($resource_type);}
 
 # Create a new collection?
 if ($collection_add==-1)