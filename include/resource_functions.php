c5894f5ab2ab401ed32432ecc79f8d5fd1f0e388
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2009-12-22 00:09:04 +0000

    removed another usage of title and country if necessary
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1619 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -776,8 +776,12 @@ function copy_resource($from,$resource_type=-1)
 		$joins_sql.=",field$join ";
 	}
 	
+	global $use_resource_column_data;
+	$add="";
+	if ($use_resource_column_data){$add="title,country,";}
+	
 	# First copy the resources row
-	sql_query("insert into resource(title,resource_type,creation_date,rating,country,archive,access,created_by $joins_sql) select title," . (($resource_type==-1)?"resource_type":("'" . $resource_type . "'")) . ",creation_date,rating,country,archive,access,created_by $joins_sql from resource where ref='$from';");
+	sql_query("insert into resource($add resource_type,creation_date,rating,archive,access,created_by $joins_sql) select $add" . (($resource_type==-1)?"resource_type":("'" . $resource_type . "'")) . ",creation_date,rating,archive,access,created_by $joins_sql from resource where ref='$from';");
 	$to=sql_insert_id();
 	
 	# Copying a resource of the 'pending review' state? Notify, if configured.
