d60cec5ed49d887961a77f1fc2ed240be3879e4d
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-05-25 20:55:28 +0000

    trying to fix my mistake
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1899 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/transform/hooks/all.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-function HookAnnotateAllModifyselect(){
-return (" ,r.annotation_count ");
-
-}
-?>
--- plugins/transform/languages/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang['annotate']="Annotate";
-$lang['annotateimage']="Annotate Image";
-$lang['annotateblurb']="To annotate the image, click 'Add Note'. You can move and resize the annotation box once you have created it. Enter your annotation and press Save.";
-
-
-?>
