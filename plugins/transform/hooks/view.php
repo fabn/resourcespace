76a506a01d1f5765ebe1aa47b4cf5be7f7bc3dd5
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-05-25 20:18:27 +0000

    annotate update omitting pdfs
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1896 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/transform/hooks/view.php
+++ plugins/transform/hooks/view.php
@@ -1,6 +1,6 @@
 <?php
 
-function HookTransformViewAfterresourceactions (){
+function HookAnnotateViewAfterresourceactions (){
 	global $ref;
 	global $access;
 	global $lang;
@@ -25,13 +25,86 @@ function HookTransformViewAfterresourceactions (){
 		}	
 	}
 
-	if ($access==0 && $resource['has_image']==1 && in_array(strtoupper($resource['file_extension']),$cropper_allowed_extensions)){
-		echo "&nbsp;&nbsp;<li><a href='../plugins/transform/pages/crop.php?ref=$ref'>&gt; ";
-		echo $lang['transform'];
+/*
+	if ($access==0 && $resource['has_image']==1 ){
+		echo "&nbsp;&nbsp;<li><a href='../plugins/annotate/pages/annotate.php?ref=$ref'>&gt; ";
+		echo $lang['annotate'];
 		echo "</a></li>";
-		return true;
 	}
+*/
+}
 
+function HookAnnotateViewRenderinnerresourcepreview(){
+	global $ref,$ffmpeg_preview_extension,$resource,$k,$search,$offset,$order_by,$sort,$archive,$lang,$download_multisize,$baseurl;
+	if ($resource['file_extension']=="pdf"){return false;}
+	
+$download_multisize=true;
+
+$flvfile=get_resource_path($ref,true,"pre",false,$ffmpeg_preview_extension);
+if (file_exists($flvfile)){return false;}
+
+if ($resource["has_image"]==1)
+	{
+		?><style type="text/css" media="all">@import "../plugins/annotate/lib/jquery/css/annotation.css";</style>
+<script type="text/javascript" src="../plugins/annotate/lib/jquery/js/jquery-1.3.2.js"></script>
+		<script type="text/javascript" src="../plugins/annotate/lib/jquery/js/jquery-ui-1.7.1.js"></script>
+		<script type="text/javascript" src="../plugins/annotate/lib/jquery/js/jquery.annotate.js"></script>
+   <script>
+     jQuery.noConflict();
+     
+</script><?php
+	$use_watermark=check_use_watermark($resource['ref']);
+	$imagepath=get_resource_path($ref,true,"pre",false,$resource["preview_extension"],-1,1,$use_watermark);
+	if (!file_exists($imagepath))
+		{
+		$imageurl=get_resource_path($ref,false,"thm",false,$resource["preview_extension"],-1,1,$use_watermark);
+		}
+	else
+		{
+		$imageurl=get_resource_path($ref,false,"pre",false,$resource["preview_extension"],-1,1,$use_watermark);
+		}
+$sizes = getimagesize($imageurl);
+$w = $sizes[0];
+$h = $sizes[1];	
+
+	?>
+	
+	<?php
+	if (file_exists($imagepath))
+		{ 
+		?>	<script language="javascript">
+			jQuery(window).load(function() {
+				jQuery("#toAnnotate").annotateImage({
+					getUrl: "<?php echo $baseurl?>/plugins/annotate/pages/get.php?ref=<?php echo $ref?>&pw=<?php echo $w?>&ph=<?php echo $h?>",
+					saveUrl: "<?php echo $baseurl?>/plugins/annotate/pages/save.php?ref=<?php echo $ref?>&pw=<?php echo $w?>&ph=<?php echo $h?>",
+					deleteUrl: "<?php echo $baseurl?>/plugins/annotate/pages/delete.php?ref=<?php echo $ref?>",
+					editable: true,
+					useAjax: true   
+				});
+			});
+		</script>
+		<div id="wrapper" style="display:block;clear:none;float:left;margin: 0px 10px 10px 0px;">
+<div>
+<img id="toAnnotate" src="<?php echo $imageurl?>" id="previewimage" class="Picture" GALLERYIMG="no" style="display:block;"   />
+	</div>
+	<br>&nbsp;&nbsp;<a style="display:inline;" href="preview.php?ref=<?php echo $ref?>&ext=<?php echo $resource["preview_extension"]?>&k=<?php echo $k?>&search=<?php echo urlencode($search)?>&offset=<?php echo $offset?>&order_by=<?php echo $order_by?>&sort=<?php echo $sort?>&archive=<?php echo $archive?>" title="<?php echo $lang["fullscreenpreview"]?>"><?php echo $lang["fullscreenpreview"]?></a>
+
+	</div>
+<?php 
+		} 
+	?><?php
+	}
+else
+	{
+	?>
+	<img src="../gfx/<?php echo get_nopreview_icon($resource["resource_type"],$resource["file_extension"],false)?>" alt="" class="Picture" style="border:none;" id="previewimage" />
+	<?php
+	}
+	
+
+	
+	
+return true;	
 }
 
 ?>
--- plugins/transform/include/config.default.php
+++ plugins/transform/include/config.default.php
@@ -1,12 +1,12 @@
 <?php
-$cropper_default_target_format = 'JPG';
-$cropper_debug=false;
-$cropper_formatarray = array('TIF','JPG','PNG'); // output formats allowed for transform operations
-$cropper_force_original_format = false;
-$cropper_cropsize='pre';
-$cropper_custom_filename=true;
-$cropper_use_filename_as_title=true;
-$cropper_allow_scale_up = true; // if false, scaling parameters that would result in enlargement are ignored
+$annotate_default_target_format = 'JPG';
+$annotate_debug=false;
+$annotate_formatarray = array('TIF','JPG','PNG'); // output formats allowed for transform operations
+$annotate_force_original_format = false;
+$annotate_cropsize='pre';
+$annotate_custom_filename=true;
+$annotate_use_filename_as_title=true;
+$annotate_allow_scale_up = true; // if false, scaling parameters that would result in enlargement are ignored
 $cropper_rotation = true; // if true, enables flipping and rotation of images
 $cropper_transform_original = false;
 $cropper_use_repage = true; // use repage feature to remove image geometry after transformation. This is necessary for most ImageMagick-based systems to behave correctly.
