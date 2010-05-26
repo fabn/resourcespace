df28fc774d3ec648b9e4772e12bf045fdb1d05fa
ddwiggins <ddwiggins@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-05-26 22:12:53 +0000

    added optional alt_type parameter to add_alternative_file function to support new type functionality
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1909 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -1105,9 +1105,9 @@ function get_alternative_files($resource)
 	return sql_query("select ref,name,description,file_name,file_extension,file_size,creation_date,alt_type from resource_alt_files where resource='$resource'");
 	}
 	
-function add_alternative_file($resource,$name,$description="",$file_name="",$file_extension="",$file_size=0)
+function add_alternative_file($resource,$name,$description="",$file_name="",$file_extension="",$file_size=0,$alt_type='')
 	{
-	sql_query("insert into resource_alt_files(resource,name,creation_date,description,file_name,file_extension,file_size) values ('$resource','" . escape_check($name) . "',now(),'" . escape_check($description) . "','" . escape_check($file_name) . "','" . escape_check($file_extension) . "','" . escape_check($file_size) . "')");
+	sql_query("insert into resource_alt_files(resource,name,creation_date,description,file_name,file_extension,file_size,alt_type) values ('$resource','" . escape_check($name) . "',now(),'" . escape_check($description) . "','" . escape_check($file_name) . "','" . escape_check($file_extension) . "','" . escape_check($file_size) . "','" . escape_check($alt_type) . "')");
 	return sql_insert_id();
 	}
 	
