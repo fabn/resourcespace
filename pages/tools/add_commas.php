0ec81c2ffdd8d075f5e007a13c74481a63c5ebe2
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-06-15 14:07:19 +0000

    update to the add_commas fix tool, to update the resource table if necessary. To clarify, before revision 1940, dropdown fields were not written correctly with a starting comma when using batch editing. If you have a dropdown field which is also being used as a search list display field or a sort field, sorting may be innaccurate due to lack of leading commas. running pages/tools/add_commas.php should fix this.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@1943 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/add_commas.php
+++ pages/tools/add_commas.php
@@ -1,24 +1,28 @@
 <?php
 
-# Quick script to fix database entries that need commas at the beginning.
+# Quick script to fix database entries that need commas at the beginning (dropdown fields edited using collection edit before r1940). 
 # When some values have commas and others don't, sorting doesn't work correctly!!!
-
-# run this and then drop the field# columns on your resource table to allow them to rebuild with new values. 
 
 include "../../include/db.php";
 include "../../include/authenticate.php"; if (!checkperm("a")) {exit("Permission denied");}
 include "../../include/general.php";
 include "../../include/resource_functions.php";
 
-$resource_type_fields=sql_query("select ref from resource_type_field where type=2 OR type=3");
+$resource_type_fields=sql_query("select ref from resource_type_field where type=3");
+$joins=get_resource_table_joins();
 
 for($n=0;$n<count($resource_type_fields);$n++){
 	$values=sql_query("select resource,resource_type_field,value from resource_data where resource_type_field=".$resource_type_fields[$n]['ref']);
 	for ($x=0;$x<count($values);$x++){
 		if (substr($values[$x]['value'],0,1) != ',')
 			{
-			echo "updating ref: ".$values[$x]['resource']. " field: ".$values[$x]['resource_type_field']. " value: ".$values[$x]['value']."<br>";
+			echo "updating resource_data- ref: ".$values[$x]['resource']. ", field: ".$values[$x]['resource_type_field']. ", value: ,".$values[$x]['value']."<br>";
 			sql_query("update resource_data set value=',".mysql_escape_string($values[$x]['value'])."' where resource='".$values[$x]['resource']."' and resource_type_field='".$values[$x]['resource_type_field']."'");
+			if (in_array($values[$x]['resource_type_field'],$joins)){
+				echo "updating resource- ref: ".$values[$x]['resource']. ", field".$values[$x]['resource_type_field'].", value: ,".$values[$x]['value']."<br>";
+				#echo("update resource set field".$values[$x]['resource_type_field']."=',".mysql_escape_string($values[$x]['value'])."' where ref='".$values[$x]['resource']."'");
+				sql_query("update resource set field".$values[$x]['resource_type_field']."=',".mysql_escape_string($values[$x]['value'])."' where ref='".$values[$x]['resource']."'");
+				}
 			}
 	}		
 }
