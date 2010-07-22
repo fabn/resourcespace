ded342e3f113c67579eb312f3c68ccc6537da4db
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-07-22 17:15:30 +0000

    fixed the bug that was preventing a straight usage of update_previews, and added some comments on the options available.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2013 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/update_previews.php
+++ pages/tools/update_previews.php
@@ -6,6 +6,17 @@
  * @subpackage Team_Admin
  */
 
+# options:
+# you can run the script straight, and it will try to create a preview from 1 to the ref of the current resource
+# This might not be efficient if you don't have resources at all ref numbers.
+# Alternatively, you can create a collection with the resources you'd like to preview, and pass col=[collectionid] (without brackets),
+# This will recreate previews for all resources in that collection. 
+# previewbased=true is an option that can help preserve alternative previews, 
+# Recreating previews would normally use the original file and overwrite alternative previews that have been uploaded,
+# but with previewbased=true, it will try to find a suitable large preview image to generate the smaller versions from.
+# If you want to recreate preview for a single resource, you can pass ref=[ref]&only=true
+
+
 include "../../include/db.php";
 include "../../include/authenticate.php"; if (!checkperm("a")) {exit("Permission denied");}
 include "../../include/general.php";
@@ -32,7 +43,8 @@ $collectionid=getvalescaped("col", false);
 $max=sql_value("select max(ref) value from resource",0);
 $ref=getvalescaped("ref",false);
 $previewbased=getvalescaped("previewbased",false);
-if ($collectionid === false){
+
+if ($collectionid == false){
     if ($ref===false) $ref = 1;
     if (update_preview($ref)){
     	?>
@@ -43,7 +55,6 @@ if ($collectionid === false){
     	{
     	echo "Skipping $ref";
     	}
-    
     if ($ref<$max && getval("only","")=="")
     	{
     	?>
