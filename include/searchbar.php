ac77121529722c3f173fa7aec9904157da98870b
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-07-26 09:17:14 +0000

    Indent checkboxes slightly when displaying 'all' to make it obvious that 'all' is a superset (visual cue)
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2017 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/searchbar.php
+++ include/searchbar.php
@@ -105,7 +105,7 @@ if (!$basic_simple_search)
 	$clear_function="";
 	$types=get_resource_types();for ($n=0;$n<count($types);$n++)
 		{
-		?><div class="tick"><input class="tickbox" id="TickBox<?php echo $n?>" type="checkbox" name="resource<?php echo $types[$n]["ref"]?>" value="yes" <?php if (((count($rt)==1) && ($rt[0]=="")) || (in_array($types[$n]["ref"],$rt))) {?>checked="true"<?php } ?> 	<?php if ($searchbar_selectall){?>onClick="resetTickAll();"<?php }?>/>&nbsp;<?php echo $types[$n]["name"]?></div><?php	
+		?><div class="tick"><?php if ($searchbar_selectall){ ?>&nbsp;&nbsp;<?php } ?><input class="tickbox" id="TickBox<?php echo $n?>" type="checkbox" name="resource<?php echo $types[$n]["ref"]?>" value="yes" <?php if (((count($rt)==1) && ($rt[0]=="")) || (in_array($types[$n]["ref"],$rt))) {?>checked="true"<?php } ?> 	<?php if ($searchbar_selectall){?>onClick="resetTickAll();"<?php }?>/>&nbsp;<?php echo $types[$n]["name"]?></div><?php	
 		$clear_function.="document.getElementById('TickBox" . $n . "').checked=true;";
 		if ($searchbar_selectall) {$clear_function.="resetTickAll();";}
 		}
