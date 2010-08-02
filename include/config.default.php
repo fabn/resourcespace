0909ef7477b4572a9cd6a8c794e4dac0efd282ce
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-08-02 15:54:16 +0000

    replaced dejavusans with dejavusanscondensed as it is likely better for contact sheets
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2030 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/config.default.php
+++ include/config.default.php
@@ -474,7 +474,7 @@ $contact_sheet_previews=true;
 # Ajax previews in contact sheet, preview image size in pixels. 
 $contact_sheet_preview_size="250x250";
 # Select a contact sheet font. Default choices are 
-# helvetica,times,courier (standard) and dejavusans for more Unicode support (but embedding/subsetting makes it slower).
+# helvetica,times,courier (standard) and dejavusanscondensed for more Unicode support (but embedding/subsetting makes it slower).
 # There are also several other fonts included in the tcpdf lib (but not ResourceSpace), which provide unicode support
 # To embed more elaborate fonts, acquire the files from the TCPDF distribution or create your own using TCPDF utilities, and install them in the lib/tcpdf/fonts folder.
 $contact_sheet_font="helvetica";
