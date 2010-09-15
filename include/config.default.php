25e7e6ff4b07dd71ac715a4d78f05e0a677e2f78
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-09-15 22:46:52 +0000

    added ods and xls to unoconv extensions, since unoconv does handle them, and the method runs after qlpreview
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2093 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/config.default.php
+++ include/config.default.php
@@ -1369,7 +1369,7 @@ $local_ftp_upload_folder = 'upload/';
 ## $unoconv_path="/usr/bin";
 # Files with these extensions will be passed to unoconv (if enabled above) for conversion to PDF and auto thumb-preview generation.
 # Default list taken from http://svn.rpmforge.net/svn/trunk/tools/unoconv/docs/formats.txt
-$unoconv_extensions=array("doc","docx","odt","odp","html","rtf","txt","ppt","pptx","sxw","sdw","html","psw","rtf","sdw","pdb","bib","txt","ltx","sdd","sda","odg","sdc");
+$unoconv_extensions=array("ods","xls","doc","docx","odt","odp","html","rtf","txt","ppt","pptx","sxw","sdw","html","psw","rtf","sdw","pdb","bib","txt","ltx","sdd","sda","odg","sdc");
 
 # Uncomment to set a point in time where collections are considered 'active' and appear in the drop-down. 
 # This is based on creation date for now. Older collections are effectively 'archived', but accessible through Manage My Collections.
