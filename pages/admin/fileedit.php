1cfbedbacafaaf112e0e90e29a951abf5217a725
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-09-17 21:14:59 +0000

    attempting fix for magic quotes, which needs to be off for web config
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2107 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/admin/fileedit.php
+++ pages/admin/fileedit.php
@@ -2,6 +2,24 @@
 include "../../include/db.php";?>
 <?php include "../../include/authenticate.php"; if (!checkperm("a")) {exit("Permission denied");}?>
 <?php
+
+if (get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripslashes($k)];
+            } else {
+                $process[$key][stripslashes($k)] = stripslashes($v);
+            }
+        }
+    }
+    unset($process);
+}
+
+
 $folder=getval("folder","default");
 
 $file=getval("file","");
