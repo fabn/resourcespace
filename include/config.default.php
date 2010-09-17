b73712faf281774d2c550a85516eff7f8231b9e5
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-09-17 20:56:10 +0000

    config notes for experimental config.php editor in System Setup, and set to false.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2106 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/config.default.php
+++ include/config.default.php
@@ -1549,4 +1549,7 @@ $direct_download_allow_ie7=false; // ie7 blocks initial downloads but after allo
 $direct_download_allow_ie8=false; // ie7 blocks initial downloads but after allowing once, it seems to work, so this option is available (no guarantees).
 $direct_download_allow_opera=false; // opera can also allow popups, but this is recommended off as well since by default it won't work for most users.
 
-$web_config_edit=true;
+# web-based config.php editing, using CodeMirror for highlighting. 
+# must have gpc_magic_quotes=Off in PHP, and make config.php writable. Magic quotes is deprecated so this is suggested.
+# this configuration is suggested to be turned on only for the Super Admin group, and note that caution must be used not to break syntax, or else you must edit the file server side to fix the site.
+$web_config_edit=false;
