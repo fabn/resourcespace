b55eeb9b26e0480d5be72eb6e9404dbd7b113e4e
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-10-14 17:49:34 +0000

    style the swf object as if it was a picture, fixes margins around the swf preview
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2161 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/swf_play.php
+++ pages/swf_play.php
@@ -12,6 +12,7 @@ $colour="505050";
 if ($theme=="greyblu") {$colour="446693";}
 
 ?>
-
+<div class="Picture">
 <OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0">
 <PARAM NAME=movie VALUE="<?php echo $swfpath?>"> <PARAM NAME=quality VALUE=high> <PARAM NAME=bgcolor VALUE=#<?php echo $colour?>> <EMBED src="<?php echo $swfpath?>" quality=high WIDTH="400" HEIGHT="300" NAME="<?php echo basename($swfpath)?>" ALIGN="left" TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED> </OBJECT> 
+</div>
