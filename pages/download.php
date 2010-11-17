b432887e2325300f8dc4225ef9a0d8ddcb354128
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2010-11-17 05:46:56 +0000

    fix for undefined variable exiftool_path in download.php
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2196 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/download.php
+++ pages/download.php
@@ -130,11 +130,13 @@ $mime="application/octet-stream";
 if ($noattach=="")
 	{		
 	# Get mime type via exiftool if possible
-	if (file_exists(stripslashes($exiftool_path) . "/exiftool") || file_exists(stripslashes($exiftool_path) . "/exiftool.exe"))
-		{	
-		$command=$exiftool_path."/exiftool -s -s -s -t -mimetype " . escapeshellarg($path);
-		$mime=shell_exec($command);
-		}	
+	if (isset($exiftool_path)){
+		if (file_exists(stripslashes($exiftool_path) . "/exiftool") || file_exists(stripslashes($exiftool_path) . "/exiftool.exe"))
+			{	
+			$command=$exiftool_path."/exiftool -s -s -s -t -mimetype " . escapeshellarg($path);
+			$mime=shell_exec($command);
+			}	
+	}	
 	# Override or correct for lack of exiftool with config mappings	
 	if (isset($mime_type_by_extension[$ext]))
 		{
