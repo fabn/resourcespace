3f49b1778f66a0e25cc1a7b9fe73c54abc5ce5ee
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-01-20 10:23:20 +0000

    Language text needed for last commit
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2287 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- languages/en.php
+++ languages/en.php
@@ -1212,4 +1212,7 @@ $lang["can_access_system_setup"]="Can access the System Setup area";
 $lang["can_change_own_password"]="Can change own account password";
 $lang["can_manage_users_in_children_groups"]="Can manage users in children groups to the user's group only";
 $lang["can_email_resources_to_own_and_children_and_parent_groups"]="Can email resources to users in the user's own group, children groups and parent group only";
+
+$lang["nodownloadcollection"]="You do not have access to download any of the resources in this collection.";
+
 ?>
