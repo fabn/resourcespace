5a4772070c6e031ea2a4b7fce30772230cb3136d
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-02-13 22:33:25 +0000

    Refine Results Plugin: Added a Swedish language file (complete).
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2344 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/refineresults/languages/sv.php
@@ -0,0 +1,11 @@
+<?php
+# Swedish
+# Language File for the Refine Results Plugin
+# Updated by Henrik Frizén 20110213 for svn r2343
+# -------
+#
+#
+$lang["refine"]="Förfina";
+$lang["refineresults"]="Sök inom detta resultatsätt";
+$lang["additionalkeywords"]="Ytterligare sökord";
+?>
