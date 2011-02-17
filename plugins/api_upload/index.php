7736735dd5f8fbbbef94c0b3ac2bb6b6f0ebf407
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-02-17 03:15:17 +0000

    an example form for using api_upload
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2373 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/api_upload/index.php
+++ plugins/api_upload/index.php
@@ -15,25 +15,24 @@ if (!in_array("api_upload",$plugins)){die("no access");}
 if (isset($_FILES['userfile'])){
  $ref=create_resource(getval("resourcetype",1),getval("archive",0),$userref);
  $path_parts=pathinfo($_FILES['userfile']['name']);
- $extension=$path_parts['extension'];  
+ $extension=strtolower($path_parts['extension']);  
  $filepath=get_resource_path($ref,true,"",true,$extension);
+
+ print_r($_FILES);
  $result=move_uploaded_file($_FILES['userfile']['tmp_name'], $filepath);
- sql_query("update resource set file_extension='$extension',preview_extension='jpg',file_modified=now() ,has_image=0 where ref='$ref'");
+ $wait=sql_query("update resource set file_extension='$extension',preview_extension='jpg',file_modified=now() ,has_image=0 where ref='$ref'");
  # Store original filename in field, if set
  global $filename_field;
  if (isset($filename_field))
     {
-    update_field($ref,$filename_field,$_FILES['userfile']['name']);	
+    $wait=update_field($ref,$filename_field,$_FILES['userfile']['name']);	
     }
 
- global $filename_field;
- $original_filename=get_data_by_field($ref,$filename_field);
-
  // extract metadata
- extract_exif_comment($ref,$extension);
+ $wait=extract_exif_comment($ref,$extension);
 
  //create previews
- create_previews($ref,false,$extension);
+ $wait=create_previews($ref,false,$extension);
  echo "resource created";
 }
 
--- plugins/api_upload/readme.txt
+++ plugins/api_upload/readme.txt
@@ -14,6 +14,8 @@ an example of a curl post to upload a file:
             $ch = curl_init();
             $post['key']='ZX13fHxpeDA6fTwtYiotIHQjImEzKjgtM3p_LXMgKDVjITosNSA,';
             $post['userfile'] = "@/var/www/test.png";
+            $post['resourcetype'] = 1;
+            $post['archive'] = 0;
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
             curl_setopt($ch, CURLOPT_URL, 'http://localhost/r2000/plugins/api_upload/index.php?');
@@ -25,3 +27,12 @@ an example of a curl post to upload a file:
 
 echo $responseBody;
 ?>
+
+example of an upload form:
+
+<form method="post" enctype="multipart/form-data" action="http://localhost/r2000/plugins/api_upload/index.php" method="POST">
+<input type="hidden" name="key" value="ZX13fHxpeDA6fTwtYiotIHQjImEzKjgtM3p_LXMgKDVjITosNSA," />
+<input type="hidden" name="resourcetype" value=3/>
+Choose a file to upload: <input name="userfile" id="userfile" type="file" /><br />
+<input type="submit" value="Upload File" />
+</form>
