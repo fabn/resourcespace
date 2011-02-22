083327645d1989d4c5b99fadce206921d020ad17
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-02-22 15:22:58 +0000

    Added the ability to navigate a results set from the resource log screen.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2394 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/log.php
+++ pages/log.php
@@ -3,6 +3,7 @@ include "../include/db.php";
 include "../include/authenticate.php";
 include "../include/general.php";
 include "../include/resource_functions.php";
+include "../include/search_functions.php";
 
 $ref=getvalescaped("ref","",true);
 
@@ -13,17 +14,70 @@ $offset=getvalescaped("offset",0,true);
 $restypes=getvalescaped("restypes","");
 if (strpos($search,"!")!==false) {$restypes="";}
 $archive=getvalescaped("archive",0,true);
-
+$starsearch=getvalescaped("starsearch","");
 $default_sort="DESC";
 if (substr($order_by,0,5)=="field"){$default_sort="ASC";}
 $sort=getval("sort",$default_sort);
+
+# next / previous resource browsing
+$go=getval("go","");
+if ($go!="")
+	{
+	$origref=$ref; # Store the reference of the resource before we move, in case we need to revert this.
+	
+	# Re-run the search and locate the next and previous records.
+	$result=do_search($search,$restypes,$order_by,$archive,240+$offset+1,$sort,false,$starsearch);
+	if (is_array($result))
+		{
+		# Locate this resource
+		$pos=-1;
+		for ($n=0;$n<count($result);$n++)
+			{
+			if ($result[$n]["ref"]==$ref) {$pos=$n;}
+			}
+		if ($pos!=-1)
+			{
+			if (($go=="previous") && ($pos>0)) {$ref=$result[$pos-1]["ref"];}
+			if (($go=="next") && ($pos<($n-1))) {$ref=$result[$pos+1]["ref"];if (($pos+1)>=($offset+72)) {$offset=$pos+1;}} # move to next page if we've advanced far enough
+			}
+		else
+			{
+			?>
+			<script type="text/javascript">
+			alert('<?php echo $lang["resourcenotinresults"] ?>');
+			</script>
+			<?php
+			}
+		}
+	# Check access permissions for this new resource, if an external user.
+	$newkey=hook("nextpreviewregeneratekey");
+	if (is_string($newkey)) {$k=$newkey;}
+	if ($k!="" && !check_access_key($ref,$k)) {$ref=$origref;} # cancel the move.
+	}
+
 
 include "../include/header.php";
 ?>
 <div class="BasicsBox">
 <p><a href="view.php?ref=<?php echo $ref?>&search=<?php echo urlencode($search)?>&offset=<?php echo $offset?>&order_by=<?php echo $order_by?>&sort=<?php echo $sort?>&archive=<?php echo $archive?>">&lt;&nbsp;<?php echo $lang["backtoresourceview"]?></a></p>
-<h1><?php echo $lang["resourcelog"]?></h1>
+
+
+<div class="backtoresults">
+<a href="log.php?ref=<?php echo $ref?>&search=<?php echo urlencode($search)?>&offset=<?php echo $offset?>&order_by=<?php echo $order_by?>&sort=<?php echo $sort?>&archive=<?php echo $archive?>&k=<?php echo $k ?>&go=previous&<?php echo hook("nextpreviousextraurl") ?>">&lt;&nbsp;<?php echo $lang["previousresult"]?></a>
+<?php 
+hook("viewallresults");
+if ($k=="") { ?>
+|
+<a href="search.php?search=<?php echo urlencode($search)?>&offset=<?php echo $offset?>&order_by=<?php echo $order_by?>&sort=<?php echo $sort?>&archive=<?php echo $archive?>&k=<?php echo $k?>"><?php echo $lang["viewallresults"]?></a>
+<?php } ?>
+|
+<a href="log.php?ref=<?php echo $ref?>&search=<?php echo urlencode($search)?>&offset=<?php echo $offset?>&order_by=<?php echo $order_by?>&sort=<?php echo $sort?>&archive=<?php echo $archive?>&k=<?php echo $k?>&go=next&<?php echo hook("nextpreviousextraurl") ?>"><?php echo $lang["nextresult"]?>&nbsp;&gt;</a>
 </div>
+
+<h1><?php echo $lang["resourcelog"] . " : " . $lang["resourceid"] . " " .  $ref ?></h1>
+
+</div>
+
 
 <div class="Listview">
 <table border="0" cellspacing="0" cellpadding="0" class="ListviewStyle">
