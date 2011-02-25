eedf65bfd2ca7d76b5448e16954d8f307ec03b6c
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-02-25 21:18:17 +0000

    multilingualize 'Select' option in ccollection compact styled actions.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2422 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/search.php
+++ pages/search.php
@@ -498,7 +498,7 @@ if (true) # Always show search header now.
     ?><?php if (substr($search,0,11)=="!collection"){?><form method="get" name="colactions" id="colactions">
     <div class="SearchItem" style="padding:0;margin:0;"><?php echo $lang['actions']?>:
     <select class="SearchWidth" name="colactionselect" onchange="if (colactions.colactionselect.options[selectedIndex].value!=''){top.main.location.href=colactions.colactionselect.options[selectedIndex].value;} colactions.colactionselect.value='';">
-    <option value="">Select...</option>
+    <option value=""><?php echo $lang['select'];?></option>
     <?php if ((!collection_is_research_request($usercollection)) || (!checkperm("r"))) { ?>
 	<?php if ($contact_sheet==true && $collections_compact_style) { ?><option value="contactsheet_settings.php?ref=<?php echo $usercollection?>"><?php echo $lang["contactsheet"]?></option><?php } ?>
     <?php if ($allow_share) { ?><option value="collection_share.php?ref=<?php echo $usercollection?>"><?php echo $lang["share"]?></option><?php } ?>
