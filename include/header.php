83a2eae3e39156a2b336434c269fab4ea79b222a
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-02-25 18:26:04 +0000

    fix for preview_all.php to remove the sidebar positioning modifications on ie7
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2419 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/header.php
+++ include/header.php
@@ -35,7 +35,7 @@ http://www.resourcespace.org/
 <?php if (!hook("adjustcolortheme")){ ?>
 <link href="<?php echo $baseurl?>/css/Col-<?php echo (isset($userfixedtheme) && $userfixedtheme!="")?$userfixedtheme:getval("colourcss",$defaulttheme)?>.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css" media="screen,projection,print" id="colourcss" />
 <?php } ?>
-<!--[if lte IE 7]> <link href="<?php echo $baseurl?>/css/globalIE.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css"  media="screen,projection,print" /> <![endif]-->
+<?php if ($pagename!="preview_all"){?><!--[if lte IE 7]> <link href="<?php echo $baseurl?>/css/globalIE.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css"  media="screen,projection,print" /> <![endif]--><?php } ?>
 <!--[if lte IE 5.6]> <link href="<?php echo $baseurl?>/css/globalIE5.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css"  media="screen,projection,print" /> <![endif]-->
 <script src="<?php echo $baseurl?>/lib/js/prototype.js?css_reload_key=<?php echo $css_reload_key?>" type="text/javascript"></script>
 <script src="<?php echo $baseurl?>/lib/js/scriptaculous.js?css_reload_key=<?php echo $css_reload_key?>" type="text/javascript"></script>
