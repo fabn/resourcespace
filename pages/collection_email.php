06b620a248cee8dd20fa48193f8fabbe28211fe6
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-03-04 19:03:20 +0000

    extend the multiple collections email picker to 365px
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2443 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collection_email.php
+++ pages/collection_email.php
@@ -92,7 +92,7 @@ include "../include/header.php";
 		echo $collection["name"] ;
 	} else { ##  this select copied from collections.php 
 ?>
-		<select name="collection" multiple="multiple" size="5" class="SearchWidth" 
+		<select name="collection" multiple="multiple" size="5" class="SearchWidthExt" style="width:365px;" 
 			onchange="document.getElementById('refDiv').innerHTML = getSelected(this); " >
 		<?php
 		$list=get_user_collections($userref);
