757d24dd553e161352504fee9a3f9de3a51c4785
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-03-08 20:41:24 +0000

    fix for compact collections interface to use the active collection in search if necessary rather than the usercollection
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2464 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collections_compact_style.php
+++ pages/collections_compact_style.php
@@ -1,5 +1,11 @@
 <?php
 // create a compact collections actions selector
+if (isset($search)){
+    $collection=substr($search,11);
+} else {
+    $collection=$usercollection;
+}
+
 ?>
 <input type=hidden name="purge" id="collectionpurge" value="">
 <style type="text/css">
@@ -14,15 +20,15 @@ else { ?>
  <select class="SearchWidth" name="colactionselect" onchange="if (colactions.colactionselect.options[selectedIndex].id=='purge'){ if (!confirm('<?php echo $lang["purgecollectionareyousure"]?>')){colactions.colactionselect.value='';return false;}}if (colactions.colactionselect.options[selectedIndex].value!=''){if (colactions.colactionselect.options[selectedIndex].id=='selectcollection'){parent.collections.location.href=colactions.colactionselect.options[selectedIndex].value;}else {top.main.location.href=colactions.colactionselect.options[selectedIndex].value;} } colactions.colactionselect.value='';">
  <?php }?>
 <option id="resetcolaction" value=""><?php echo $lang['select'];?></option>
-<?php if ($pagename=="search"){?><option id="selectcollection" value="collections.php?collection=<?php echo $usercollection?>"><?php echo $lang['selectcollection'];?></option><?php } ?>
-<?php if ((!collection_is_research_request($usercollection)) || (!checkperm("r"))) { ?>
-	<?php if ($contact_sheet==true && $collections_compact_style) { ?><option value="contactsheet_settings.php?ref=<?php echo $usercollection?>"><?php echo $lang["contactsheet"]?></option><?php } ?>
-    <?php if ($allow_share) { ?><option value="collection_share.php?ref=<?php echo $usercollection?>"><?php echo $lang["share"]?></option><?php } ?>
-    <?php if (($userref==$cinfo["user"]) || (checkperm("h"))) {?><option value="collection_edit.php?ref=<?php echo $usercollection?>"><?php echo $allow_share?$lang["action-edit"]:$lang["editcollection"]?></option><?php } ?>
-	<?php if ($preview_all){?><option value="preview_all.php?ref=<?php echo $usercollection?>"><?php echo $lang["preview_all"]?></option><?php } ?>
-    <?php if ($feedback) {?><option value="collection_feedback.php?collection=<?php echo $usercollection?>&k=<?php echo $k?>"><?php echo $lang["sendfeedback"]?></option><?php } ?>
+<?php if ($pagename=="search"){?><option id="selectcollection" value="collections.php?collection=<?php echo $collection?>"><?php echo $lang['selectcollection'];?></option><?php } ?>
+<?php if ((!collection_is_research_request($collection)) || (!checkperm("r"))) { ?>
+	<?php if ($contact_sheet==true && $collections_compact_style) { ?><option value="contactsheet_settings.php?ref=<?php echo $collection?>"><?php echo $lang["contactsheet"]?></option><?php } ?>
+    <?php if ($allow_share) { ?><option value="collection_share.php?ref=<?php echo $collection?>"><?php echo $lang["share"]?></option><?php } ?>
+    <?php if (($userref==$cinfo["user"]) || (checkperm("h"))) {?><option value="collection_edit.php?ref=<?php echo $collection?>"><?php echo $allow_share?$lang["action-edit"]:$lang["editcollection"]?></option><?php } ?>
+	<?php if ($preview_all){?><option value="preview_all.php?ref=<?php echo $collection?>"><?php echo $lang["preview_all"]?></option><?php } ?>
+    <?php if ($feedback) {?><option value="collection_feedback.php?collection=<?php echo $collection?>&k=<?php echo $k?>"><?php echo $lang["sendfeedback"]?></option><?php } ?>
     <?php } else {
-    $research=sql_value("select ref value from research_request where collection='$usercollection'",0);	
+    $research=sql_value("select ref value from research_request where collection='$collection'",0);	
 	?>
     <option value="team/team_research.php"><?php echo $lang["manageresearchrequests"]?></option>    
     <option value="team/team_research_edit.php?ref=<?php echo $research?>"><?php echo $lang["editresearchrequests"]?></option>    
@@ -30,29 +36,29 @@ else { ?>
     
     <?php 
     # If this collection is (fully) editable, then display an extra edit all link
-    if ((count($result)>0) && checkperm("e" . $result[0]["archive"]) && allow_multi_edit($usercollection)) { ?>
-    <option value="search.php?search=<?php echo urlencode("!collection" . $usercollection)?>"><?php echo $lang["viewall"]?></option>
-    <option value="edit.php?collection=<?php echo $usercollection?>"><?php echo $lang["action-editall"]?></option>
+    if ((count($result)>0) && checkperm("e" . $result[0]["archive"]) && allow_multi_edit($collection)) { ?>
+    <option value="search.php?search=<?php echo urlencode("!collection" . $collection)?>"><?php echo $lang["viewall"]?></option>
+    <option value="edit.php?collection=<?php echo $collection?>"><?php echo $lang["action-editall"]?></option>
 
     <?php } else { ?>
-    <option value="search.php?search=<?php echo urlencode("!collection" . $usercollection)?>"><?php echo $lang["viewall"]?></option>
+    <option value="search.php?search=<?php echo urlencode("!collection" . $collection)?>"><?php echo $lang["viewall"]?></option>
     <?php } ?>
     
     <?php if ($count_result>0)
     	{ 
 		# Ability to request a whole collection (only if user has restricted access to any of these resources)
-		$min_access=collection_min_access($usercollection);
+		$min_access=collection_min_access($collection);
 		if ($min_access!=0)
 			{
 		    ?>
-		    <option value="collection_request.php?ref=<?php echo $usercollection?>&k=<?php echo $k?>"><?php echo 	$lang["requestall"]?></option>
+		    <option value="collection_request.php?ref=<?php echo $collection?>&k=<?php echo $k?>"><?php echo 	$lang["requestall"]?></option>
 		    <?php
 		    }
 	    }
 	?>
     
    	<?php if (isset($zipcommand)) { ?>
-    <option value="terms.php?k=<?php echo $k?>&url=<?php echo urlencode("pages/collection_download.php?collection=" .  $usercollection . "&k=" . $k)?>"><?php echo $lang["action-download"]?></option>
+    <option value="terms.php?k=<?php echo $k?>&url=<?php echo urlencode("pages/collection_download.php?collection=" .  $collection . "&k=" . $k)?>"><?php echo $lang["action-download"]?></option>
 	<?php } ?>
     <?php if ($collection_purge){ 
         if (checkperm("e0") && $cinfo["cant_delete"] == 0) {
