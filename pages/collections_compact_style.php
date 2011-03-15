934302af1bb49b9c860101f967bed95b23f2479d
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-03-15 20:19:39 +0000

    rearranged/refactored order of tool list in collections_compact_style.php
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2486 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collections_compact_style.php
+++ pages/collections_compact_style.php
@@ -20,57 +20,107 @@ else { ?>
  <select class="SearchWidth" name="colactionselect" onchange="if (colactions.colactionselect.options[selectedIndex].id=='purge'){ if (!confirm('<?php echo $lang["purgecollectionareyousure"]?>')){colactions.colactionselect.value='';return false;}}if (colactions.colactionselect.options[selectedIndex].value!=''){if (colactions.colactionselect.options[selectedIndex].id=='selectcollection'){parent.collections.location.href=colactions.colactionselect.options[selectedIndex].value;}else {top.main.location.href=colactions.colactionselect.options[selectedIndex].value;} } colactions.colactionselect.value='';">
  <?php }?>
 <option id="resetcolaction" value=""><?php echo $lang['select'];?></option>
+<!-- select collection -->
 <?php if ($pagename=="search"){?><option id="selectcollection" value="collections.php?collection=<?php echo $collection?>"><?php echo $lang['selectcollection'];?></option><?php } ?>
+<!-- end select collection -->
+
+
+<!-- viewall -->
+<?php if ($pagename!="search"){
+    ?><option value="search.php?search=<?php echo urlencode("!collection" . $collection)?>"><?php echo $lang["viewall"]?></option>
+<?php } ?>
+<!-- end viewall -->
+
+
+<!-- preview all -->
+<?php if ($preview_all){?>
+<option value="preview_all.php?ref=<?php echo $collection?>"><?php echo $lang["preview_all"]?></option>
+<?php } ?>
+<!-- end preview_all -->
+
+
+<!-- zipall -->
+   	<?php if (isset($zipcommand)) { ?>
+    <option value="terms.php?k=<?php echo $k?>&url=<?php echo urlencode("pages/collection_download.php?collection=" .  $collection . "&k=" . $k)?>"><?php echo $lang["action-download"]?></option>
+	<?php } ?>
+<!-- end zipall -->
+
+
+<!-- edit metadata -->    
+<?php # If this collection is (fully) editable, then display an extra edit all link
+if ((count($result)>0) && checkperm("e" . $result[0]["archive"]) && allow_multi_edit($collection)) { ?>
+    <option value="edit.php?collection=<?php echo $collection?>"><?php echo $lang["action-editall"]?></option>
+<?php } ?>
+<!-- end edit metadata -->
+
+
+<!-- edit collection -->
 <?php if ((!collection_is_research_request($collection)) || (!checkperm("r"))) { ?>
-	<?php if ($contact_sheet==true && $collections_compact_style) { ?><option value="contactsheet_settings.php?ref=<?php echo $collection?>"><?php echo $lang["contactsheet"]?></option><?php } ?>
-    <?php if ($allow_share) { ?><option value="collection_share.php?ref=<?php echo $collection?>"><?php echo $lang["share"]?></option><?php } ?>
     <?php if (($userref==$cinfo["user"]) || (checkperm("h"))) {?><option value="collection_edit.php?ref=<?php echo $collection?>"><?php echo $allow_share?$lang["action-edit"]:$lang["editcollection"]?></option><?php } ?>
-	<?php if ($preview_all){?><option value="preview_all.php?ref=<?php echo $collection?>"><?php echo $lang["preview_all"]?></option><?php } ?>
-    <?php if ($feedback) {?><option value="collection_feedback.php?collection=<?php echo $collection?>&k=<?php echo $k?>"><?php echo $lang["sendfeedback"]?></option><?php } ?>
     <?php } else {
     $research=sql_value("select ref value from research_request where collection='$collection'",0);	
 	?>
     <option value="team/team_research.php"><?php echo $lang["manageresearchrequests"]?></option>    
     <option value="team/team_research_edit.php?ref=<?php echo $research?>"><?php echo $lang["editresearchrequests"]?></option>    
-	<?php } ?>
-    
-    <?php 
-    # If this collection is (fully) editable, then display an extra edit all link
-    if ((count($result)>0) && checkperm("e" . $result[0]["archive"]) && allow_multi_edit($collection)) { ?>
+<?php } ?>
+<!-- end edit collection -->
 
-    <?php if ($pagename!="search"){
-    ?><option value="search.php?search=<?php echo urlencode("!collection" . $collection)?>"><?php echo $lang["viewall"]?></option>
-    <?php } ?>
-    
-    <option value="edit.php?collection=<?php echo $collection?>"><?php echo $lang["action-editall"]?></option>
 
-    <?php } else { ?>
-    <option value="search.php?search=<?php echo urlencode("!collection" . $collection)?>"><?php echo $lang["viewall"]?></option>
-    <?php } ?>
-    
-    <?php if ($count_result>0)
-    	{ 
-		# Ability to request a whole collection (only if user has restricted access to any of these resources)
-		$min_access=collection_min_access($collection);
-		if ($min_access!=0)
-			{
-		    ?>
-		    <option value="collection_request.php?ref=<?php echo $collection?>&k=<?php echo $k?>"><?php echo 	$lang["requestall"]?></option>
-		    <?php
-		    }
-	    }
-	?>
-    
-   	<?php if (isset($zipcommand)) { ?>
-    <option value="terms.php?k=<?php echo $k?>&url=<?php echo urlencode("pages/collection_download.php?collection=" .  $collection . "&k=" . $k)?>"><?php echo $lang["action-download"]?></option>
-	<?php } ?>
-    <?php if ($collection_purge){ 
-        if (checkperm("e0") && $cinfo["cant_delete"] == 0) {
-			?><option id="purge" value="collection_manage.php?purge=<?php echo $cinfo['ref']?>"><?php echo $lang["purgeanddelete"]?></option><?php 
-		} 
-	}
-	?>
-    <?php if (($userref==$cinfo["user"]) || (checkperm("h"))) {?><option value="collection_log.php?ref=<?php echo $cinfo["ref"]?>"><?php echo $lang["log"]?></option><?php } ?>
-    <?php hook("collectiontoolcompact");?>
+<!-- contactsheet -->
+<?php if ($contact_sheet==true && $collections_compact_style) { ?>
+<option value="contactsheet_settings.php?ref=<?php echo $collection?>"><?php echo $lang["contactsheet"]?></option>
+<?php } ?>
+<!-- end contactsheet -->
+
+
+<?php hook("collectiontoolcompact");?>
+
+
+<!-- share -->
+<?php if ($allow_share) { ?>
+<option value="collection_share.php?ref=<?php echo $collection?>"><?php echo $lang["share"]?></option>
+<?php } ?>
+<!-- end share -->
+
+
+<!-- feedback -->
+<?php if ($feedback) {?>
+<option value="collection_feedback.php?collection=<?php echo $collection?>&k=<?php echo $k?>"><?php echo $lang["sendfeedback"]?></option>
+<?php } ?>
+<!-- end feedback -->
+
+
+<!-- request all -->    
+<?php if ($count_result>0)
+    { 
+    # Ability to request a whole collection (only if user has restricted access to any of these resources)
+    $min_access=collection_min_access($collection);
+    if ($min_access!=0)
+        {
+        ?>
+        <option value="collection_request.php?ref=<?php echo $collection?>&k=<?php echo $k?>"><?php echo 	$lang["requestall"]?></option>
+        <?php
+        }
+    }
+?>
+<!-- end request all -->
+
+
+<!-- purge -->
+<?php if ($collection_purge){ 
+    if (checkperm("e0") && $cinfo["cant_delete"] == 0) {
+        ?><option id="purge" value="collection_manage.php?purge=<?php echo $cinfo['ref']?>"><?php echo $lang["purgeanddelete"]?></option><?php 
+    } 
+} ?>
+<!-- end purge -->
+
+
+<!-- log -->
+<?php if (($userref==$cinfo["user"]) || (checkperm("h"))) {?>
+    <option value="collection_log.php?ref=<?php echo $cinfo["ref"]?>"><?php echo $lang["log"]?></option>
+<?php } ?>
+<!-- end log -->
+
+
     </select><?php if ($pagename!="search"){?><?php if ($thumbs=="show") { ?><br /><br /><a href="collections.php?thumbs=hide" onClick="ToggleThumbs();">&gt;&nbsp;<?php echo $lang["hidethumbnails"]?></a><?php } ?><?php if ($thumbs=="hide") { ?>&nbsp;&nbsp;&nbsp;<a href="collections.php?thumbs=show" onClick="ToggleThumbs();">&gt;&nbsp; <?php echo $lang["showthumbnails"]?></a><?php } ?></div><?php } ?>
 </form>
