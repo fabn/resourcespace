c83daec160c88f2a3589b9b7eaf14650d65faba0
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-03-25 20:40:32 +0000

    add link to api_core plugin through the change_password page
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2528 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/api_core/hooks/change_password.php
@@ -0,0 +1,5 @@
+<?php
+function HookApi_coreChange_passwordAfterchangepasswordform(){
+	global $lang,$baseurl;
+	?>&gt;&nbsp;<a href="<?php echo $baseurl?>/plugins/api_core/index.php"><?php echo $lang["apiaccess"];?></a><?php
+}
