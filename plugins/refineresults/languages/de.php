9f6140176ee141d8cbbb5e7f9a0161945726d19d
stefan <stefan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-03-28 07:30:59 +0000

    Added German translation for refineresults plugin.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2532 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/refineresults/languages/de.php
@@ -0,0 +1,11 @@
+<?php
+# German
+# Language File for the Refine Results Plugin
+# Updated by Stefan Wild 20110328 for svn r2531
+# -------
+#
+#
+$lang["refine"]="Suchen";
+$lang["refineresults"]="In diesen Suchergebnissen suchen";
+$lang["additionalkeywords"]="Zusätzliche Stichworte";
+?>
