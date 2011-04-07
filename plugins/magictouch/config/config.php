cd8139ff3c19af62bfe2541f59c3757acf088705
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-04-07 01:35:33 +0000

    removed indd from magictouch and annotate exclusion defaults
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2582 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/magictouch/config/config.php
+++ plugins/magictouch/config/config.php
@@ -2,7 +2,7 @@
 $magictouch_account_id='';
 $magictouch_secure='https'; 
 $magictouch_rt_exclude=array(3,4,8,13,18,21,24);
-$magictouch_ext_exclude=array("pdf","odt","indd");
+$magictouch_ext_exclude=array("pdf","odt");
 
 // sizes to attempt for zoomed file, in order.
 // note that watermarks don't apply above scr. 
