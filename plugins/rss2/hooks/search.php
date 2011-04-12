023bdbad3538145be111ab8e911185c069140153
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-04-12 08:54:58 +0000

    fix for rss2, don't show the bottom link for external shares
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2602 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/rss2/hooks/search.php
+++ plugins/rss2/hooks/search.php
@@ -6,7 +6,7 @@ function HookRss2SearchResultsbottomtoolbar()
  	global $userpassword,$username,$api_scramble_key;
     $apikey=make_api_key($username,$userpassword);
 	$skey = md5($api_scramble_key.$apikey.$search.$archive); 
-
+    global $k; if ($k!=""){return false;}
 ?>
 <div class="InpageNavLeftBlock"><a target="_top" href="<?php echo $baseurl?>/plugins/rss2/pages/rssfilter.php?key=<?php echo $apikey;?>&search=<?php echo urlencode($search)?>&restypes=<?php echo urlencode($restypes)?>&archive=<?php echo $archive?>&skey=<?php echo urlencode($skey); ?>">&gt;&nbsp;<?php echo $lang["rss_feed_for_search_filter"]; ?></a></div>
 <?php
