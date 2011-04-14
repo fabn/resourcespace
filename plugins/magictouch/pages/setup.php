3d17ebb7372a0d01c7b9731b5d80c14f02ef402d
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-04-14 16:30:42 +0000

    improved setup page for magictouch. added extensions/resource type exclusions, and view and preview sizes for larger images.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2615 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/magictouch/pages/setup.php
+++ plugins/magictouch/pages/setup.php
@@ -4,16 +4,39 @@ include "../../../include/authenticate.php"; if (!checkperm("u")) {exit ("Permis
 include "../../../include/general.php";
 
 
+if (defined("PHP_EOL")) {$eol=PHP_EOL;} else {$eol="\r\n";}
 if (!isset($magictouch_account_id)) {$magictouch_account_id="";}
 if (!isset($magictouch_secure)) {$magictouch_secure="http";}
+if (!isset($magictouch_ext_exclude)){$ext_exclude=array("pdf","odt");}
+if (!isset($magictouch_rt_exclude)){$rt_exclude=array(3,4,8,13,18,21,24);}
+if (!isset($magictouch_view_page_sizes)){$view_sizes=array("lpr","scr");}
+if (!isset($magictouch_preview_page_sizes)){$magictouch_preview_page_sizes=array("hpr","lpr");}
+
 
 if (getval("submit","")!="")
 	{
 	$accountid=getvalescaped("accountid","");
 	$secure=getvalescaped("secure","");
+	$ext_exclude="\"".str_replace(",","\",\"",getvalescaped("extexclude",""))."\"";
+	$view_sizes="\"".str_replace(",","\",\"",getvalescaped("viewsizes",""))."\"";
+	$preview_sizes="\"".str_replace(",","\",\"",getvalescaped("previewsizes",""))."\"";
+	if (isset($_POST['rtexclude'])){
+		$rt_exclude=$_POST['rtexclude'];
+	}
+	else {
+		$rt_exclude=array();
+	}
 	
+	$secure=getvalescaped("secure","");
 	$f=fopen("../config/config.php","w");
-	fwrite($f,"<?php \$magictouch_account_id='$accountid';\$magictouch_secure='$secure'; ?>");
+	fwrite($f,"<?php \$magictouch_account_id='$accountid';$eol\$magictouch_secure='$secure';$eol");
+	if (count($rt_exclude)!=0){
+		$rt_exclude=implode(",",$rt_exclude);
+	}
+	fwrite($f,"\$magictouch_rt_exclude=array(".$rt_exclude.");$eol");
+	fwrite($f,"\$magictouch_ext_exclude=array(".$ext_exclude.");$eol");
+	fwrite($f,"\$magictouch_view_page_sizes=array(".$view_sizes.");$eol");
+	fwrite($f,"\$magictouch_preview_page_sizes=array(".$preview_sizes.");$eol");
 	fclose($f);
 	redirect("pages/team/team_home.php");
 	}
@@ -24,6 +47,9 @@ include "../../../include/header.php";
 <div class="BasicsBox"> 
   <h2>&nbsp;</h2>
   <h1>MagicTouch Configuration</h1>
+  <?php
+if(!is_writable("../config/config.php")){echo("MagicTouch config.php is not writable: chmod 777 plugins/magictouch/config/config.php");die();}
+?>
 
   <div class="VerticalNav">
  <form id="form1" name="form1" method="post" action="">
@@ -35,7 +61,27 @@ include "../../../include/header.php";
    <option <?php if ($magictouch_secure=="https") { ?>selected<?php } ?>>https</option>
    </select>
    </p>
+   <p><label for="extexclude">Extensions to exclude (comma separated):</label>
+   <input name="extexclude" type="text" value="<?php echo implode(',',$magictouch_ext_exclude); ?>" size="30" />
+   </p>
+   <p><label for="rtexclude[]">Resource Types to exclude (highlight to exclude):</label> <br />
+   <?php $rtypes=get_resource_types();?>
+   <select name="rtexclude[]" multiple="multiple" size="7">
+	<?php foreach($rtypes as $rt){?>
+	<option value="<?php echo $rt['ref']?>" <?php if (in_array($rt['ref'],$magictouch_rt_exclude)){?>selected<?php } ?>><?php echo $rt['name']?></option>
+	<?php } ?>
+   </select>
 
+   <p><label for="viewsizes">View page sizes (in order, which sizes to check for the magictouch larger preview):</label> <br />
+   <?php $sizes=sql_query("select * from preview_size");?>
+      <input name="viewsizes" type="text" value="<?php echo implode(',',$magictouch_view_page_sizes); ?>" size="30" />
+   </p>
+   
+   <p><label for="previewsizes">Preview page sizes (in order, which sizes to check for the magictouch larger preview):</label> <br />
+   <?php $sizes=sql_query("select * from preview_size");?>
+      <input name="previewsizes" type="text" value="<?php echo implode(',',$magictouch_preview_page_sizes); ?>" size="30" />
+   </p>
+   
 <input type="submit" name="submit" value="<?php echo $lang["save"]?>">   
 
 
