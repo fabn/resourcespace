8693d75674b18500de3b54fe5417cd0a2839b2fe
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-05-13 00:13:42 +0000

    reverting last two commits...this solution doesn't work
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2690 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/refineresults/languages/en.php
+++ plugins/refineresults/languages/en.php
@@ -3,6 +3,6 @@
 $lang["refine"]="Refine";
 $lang["refineresults"]="Search within these results";
 $lang["additionalkeywords"]="Additional Keywords";
-$lang["returntooriginalresults"]="Return to original results";
+
 
 ?>
