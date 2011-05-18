2b566df5814d53a2e4df75926c5e102b800f63c0
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-05-18 03:05:58 +0000

    mp3 player volume should be 100
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2697 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/mp3_play.php
+++ pages/mp3_play.php
@@ -13,7 +13,7 @@ if ($theme=="black") {$bgcolor1="666666";$bgcolor2="111111";$buttoncolor="999999
 <td><h2><?php echo $lang["preview"] ?></h2></td>
 <td align="center" colspan="2"><?php } ?><center><object type="application/x-shockwave-flash" data="../lib/flashplayer/player_mp3_maxi.swf" width="200" height="20" <?php if ($pagename=="search"){?>style="margin:0px;margin-top:-20px;margin-left:auto;margin-right:auto;"<?php }?>>
 <param name="movie" value="../lib/flashplayer/player_mp3_maxi.swf" />
-<param name="FlashVars" value="mp3=<?php echo $mp3path?>&width=200&buttoncolor=<?php echo $buttoncolor?>&playercolor=<?php echo $color?>&bgcolor=<?php echo $color?>&bgcolor1=<?php echo $bgcolor1?>&bgcolor2=<?php echo $bgcolor2?>&volume=200&showvolume=1" /></object></center><?php if ($pagename!="search"){?>
+<param name="FlashVars" value="mp3=<?php echo $mp3path?>&width=200&buttoncolor=<?php echo $buttoncolor?>&playercolor=<?php echo $color?>&bgcolor=<?php echo $color?>&bgcolor1=<?php echo $bgcolor1?>&bgcolor2=<?php echo $bgcolor2?>&volume=100&showvolume=1" /></object></center><?php if ($pagename!="search"){?>
 </td>
 </tr>
 <?php } ?>
