301c22b4c4b3424056caf8de571538f1cffab9a8
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-05-20 19:08:01 +0000

    callback to focus on input when expanding the refineresults div
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2717 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/refineresults/hooks/all.php
+++ plugins/refineresults/hooks/all.php
@@ -8,9 +8,12 @@ function HookRefineresultsSearchBeforesearchresults()
 	#if (substr($search,0,1)=="!") {return false;} # Only work for normal (non 'special') searches
 	?>
 	<div class="SearchOptionNav"><a href="#" onClick="
+	   function OnFinish(){
+        $('refine_keywords').focus();
+	}
 	if ($('RefinePlus').innerHTML=='+')
 		{
-		Effect.SlideDown('RefineResults',{duration:0.5});
+		Effect.SlideDown('RefineResults',{duration:0.5,afterUpdate:OnFinish});
 		$('RefinePlus').innerHTML='&minus;';
 		}
 	else
@@ -35,7 +38,7 @@ function HookRefineresultsSearchBeforesearchresultsexpandspace()
 	<form method="post">
 	<div class="Question" id="question_related" style="border-top:none;">
 	<label for="related"><?php echo $lang["additionalkeywords"]?></label>
-	<input class="stdwidth" type=text name="refine_keywords" value="">
+	<input class="stdwidth" type=text id="refine_keywords" name="refine_keywords" value="">
 	<input type=hidden name="archive" value="<?php echo $archive?>">
 	<input type=hidden name="search" value="<?php echo htmlspecialchars($search) ?>">
 	<div class="clearerleft"> </div>
