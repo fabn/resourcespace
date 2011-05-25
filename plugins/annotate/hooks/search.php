923224fcd3e3084b8ccac539e218a6adf62388b6
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-05-25 18:22:35 +0000

    fix for annotate icons in search results that caused a bomb in some cases
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2733 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/annotate/hooks/search.php
+++ plugins/annotate/hooks/search.php
@@ -3,7 +3,7 @@
 function HookAnnotateSearchIcons(){ 
 	global $result,$n,$lang;
     if (!is_array($result)){?><div class="clearerleft"></div><div class="ResourcePanelInfo"><span class="IconUserRatingSpace"></span></div><?php return true;}
-	if ($result[$n]['annotation_count']!=null && $result[$n]['annotation_count']!=0 && $result[$n]['file_extension']!="pdf"){
+	if (isset($result[$n]) && $result[$n]['annotation_count']!=null && $result[$n]['annotation_count']!=0 && $result[$n]['file_extension']!="pdf"){
 	?>
 <div class="clearerleft"></div><div class="ResourcePanelInfo"><span class="IconUserRatingSpace" style="width:0px;"></span><img src="../plugins/annotate/lib/jquery/images/asterisk_yellow.png" height="10px;"/>&nbsp;&nbsp;<?php echo $result[$n]['annotation_count']==1 ? $lang["note-1"] : str_replace("%number", $result[$n]['annotation_count'], $lang["note-2"]); ?></div><?php 
 } else { ?>
