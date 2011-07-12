22af4c9c63371dcc327391cef39ee0dbc73e43f0
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-07-12 10:23:31 +0000

    Removed Pragma: No Cache header as it caused a download issue for IE.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2833 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/db.php
+++ include/db.php
@@ -19,7 +19,6 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");  // always modified
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
 
 if (!function_exists('mysql_set_charset'))
 	{
