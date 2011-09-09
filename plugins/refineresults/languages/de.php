0f9296e2f8f36d08148f2dac4c9a0930cdc9259a
stefan <stefan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-09-09 08:09:49 +0000

    Added missing translation in refineresults plugin
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2910 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/refineresults/languages/de.php
+++ plugins/refineresults/languages/de.php
@@ -1,11 +1,12 @@
 <?php
 # German
 # Language File for the Refine Results Plugin
-# Updated by Stefan Wild 20110328 for svn r2531
+# Updated by Stefan Wild 20110909 for svn r2900
 # -------
 #
 #
 $lang["refine"]="Suchen";
 $lang["refineresults"]="In diesen Suchergebnissen suchen";
 $lang["additionalkeywords"]="Zusätzliche Stichworte";
+$lang["clearsearch"]="Suchbegriffe zurücksetzen";
 ?>
