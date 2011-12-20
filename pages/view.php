4240468dad401e2688043eca2d0e7b93c3d8195c
adoerfler <adoerfler@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2011-12-20 20:24:44 +0000

    Forgot to use i18n_get_translated() for the resources in the same collection/theme.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3132 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/view.php
+++ pages/view.php
@@ -1054,7 +1054,7 @@ if (count($result)>0)
 		for ($n=0;$n<count($result);$n++)			
 			{
 			?>
-			<li><a href="search.php?search=!collection<?php echo $result[$n]["ref"]?>"><?php echo (strlen($result[$n]["theme"])>0)?htmlspecialchars(str_replace("*","",$result[$n]["theme"]) . " / "):$lang["public"] . " : " . htmlspecialchars($result[$n]["fullname"] . " / ")?><?php echo htmlspecialchars($result[$n]["name"])?></a></li>
+			<li><a href="search.php?search=!collection<?php echo $result[$n]["ref"]?>"><?php echo (strlen($result[$n]["theme"])>0)?htmlspecialchars(str_replace("*","",i18n_get_translated($result[$n]["theme"])) . " / "):$lang["public"] . " : " . htmlspecialchars($result[$n]["fullname"] . " / ")?><?php echo htmlspecialchars(i18n_get_translated($result[$n]["name"]))?></a></li>
 			<?php		
 			}
 		?>
