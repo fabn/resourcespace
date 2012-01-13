efe85ebf4c43a1d2a43beb33e16c2eb3cc01fee2
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-01-13 19:18:00 +0000

    include plugins for java upload popup
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3151 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/upload_java_popup.php
+++ pages/upload_java_popup.php
@@ -45,6 +45,31 @@ http://www.resourcespace.org/
 <?php } ?>
 <!--[if lte IE 7]> <link href="<?php echo $baseurl?>/css/globalIE.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css"  media="screen,projection,print" /> <![endif]-->
 <!--[if lte IE 5.6]> <link href="<?php echo $baseurl?>/css/globalIE5.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css"  media="screen,projection,print" /> <![endif]-->
+
+<?php
+# Include CSS files for for each of the plugins too (if provided)
+for ($n=0;$n<count($plugins);$n++)
+	{
+	$csspath=dirname(__FILE__)."/../plugins/" . $plugins[$n] . "/css/style.css";
+	if (file_exists($csspath))
+		{
+		?>
+		<link href="<?php echo $baseurl?>/plugins/<?php echo $plugins[$n]?>/css/style.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css" media="screen,projection,print"  />
+		<?php
+		}
+	$theme=((isset($userfixedtheme) && $userfixedtheme!=""))?$userfixedtheme:getval("colourcss",$defaulttheme);
+	$csspath=dirname(__FILE__)."/../plugins/" . $plugins[$n] . "/css/Col-".$theme.".css";	
+	if (file_exists($csspath))
+		{
+		?>
+		<link href="<?php echo $baseurl?>/plugins/<?php echo $plugins[$n]?>/css/Col-<?php echo $theme?>.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css" media="screen,projection,print" id="<?php echo $plugins[$n]?>css" />
+		<?php
+		}	
+	}
+?>
+
+
+
 </head><body>
 <div id="height">
 <div id="Header">
