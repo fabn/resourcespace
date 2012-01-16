01c0c8bbfddc859cdd401785a74bcba4337477bc
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-01-16 19:00:42 +0000

    delete miscommit
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3159 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/whitespace/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-# This theme is based on "whitegry" so this variable must be set.
-$userfixedtheme="greyblu";
-$homeanim_folder="plugins/airotek/gfx/homeanim";
-$default_home_page="../plugins/airotek/pages/tag_projects.php";
-?>
