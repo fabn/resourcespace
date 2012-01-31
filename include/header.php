62eefd2ed82eb3278291b3f1ab2d479333ca3067
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-01-31 20:33:21 +0000

    fix extra space in userfullname link at top
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3178 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/header.php
+++ include/header.php
@@ -144,9 +144,7 @@ else
 	<ul>
 	<?php if (!hook("replaceheaderfullnamelink")){?>
 	<li><?php if ($allow_password_change && !checkperm("p")) { ?>
-	<a href="<?php echo $baseurl?>/pages/change_password.php"><?php } ?><?php echo $userfullname?>
-	<?php } /* end replacefullnamelink */?>
-	<?php if ($allow_password_change && !checkperm("p")) { ?></a><?php } ?></li>
+	<a href="<?php echo $baseurl?>/pages/change_password.php"><?php } ?><?php echo $userfullname?><?php } /* end replacefullnamelink */?><?php if ($allow_password_change && !checkperm("p")) { ?></a><?php } ?></li>
 	<?php hook("addtoplinks");?>
 	<li><a href="<?php echo $baseurl?>/login.php?logout=true&nc=<?php echo time()?>" target="_top"><?php echo $lang["logout"]?></a></li>
 	<?php hook("addtologintoolbarmiddle");?>
