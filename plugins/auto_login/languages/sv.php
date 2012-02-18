26b152912143647a5c2c2a5ad1c87a09a6195973
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-02-18 21:47:38 +0000

    Auto Login Plugin: Added a Swedish language file (complete).
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3209 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/auto_login/languages/sv.php
@@ -0,0 +1,13 @@
+<?php
+# Swedish
+# Language File for the Auto Login Plugin
+# Updated by Henrik Frizén 20120218 for svn r3208
+# -------
+#
+#
+$lang['auto_login_enabled']='Aktivera automatisk inloggning baserad på ip-adress?';
+$lang['auto_login_ip']='Ip-adresser för automatisk inloggning';
+
+$lang['wildcardpermittedeg']="Jokertecken kan användas i begränsningen av ip-adresser, reguljära uttryck inleds med ’!’. Exempel:";
+
+?>
