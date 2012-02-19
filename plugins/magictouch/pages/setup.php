23198ba54eefa952ebb3df6e96832d612d5cac54
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-02-19 15:34:23 +0000

    Improved the translation functionality in the magictouch plugin: Added 9 $lang[...] Replaced fixed strings with newly created $lang[...]. Made a couple of text changes to improve the usability - thanks Dan for your help! Added a Swedish language file (complete).
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3212 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/magictouch/pages/setup.php
+++ plugins/magictouch/pages/setup.php
@@ -37,28 +37,29 @@ include "../../../include/header.php";
 ?>
 <div class="BasicsBox"> 
   <h2>&nbsp;</h2>
-  <h1>MagicTouch Configuration</h1>
+  <h1><?php echo $lang["magictouch_configuration"];?></h1>
   
 <?php 
 
 
 
 if ($magictouch_account_id==""){
-	echo "You must set up MagicTouch. 
-	To get an account ID <a target='_new' href='http://www.magictoolbox.com/magictouch/signup/'>>Click Here</a>. 
-	<br /><br />Configure the account id below, and register your domain with your Magic Touch account.<br /><br />";
+	echo $lang["get-magictouch"];
+	echo "<br /><br />";
+	echo $lang["configure-account-id-and-register-domain"];
+	echo "<br /><br />";
 }?>
 
 
 <form id="form1" name="form1" method="post" action="">
 
-<?php echo config_text_field("magictouch_account_id","Account ID (from URL)",$magictouch_account_id);?>
-<?php echo config_custom_select("magictouch_secure","HTTPS?",array("https","http"),$magictouch_secure);?>
-<?php echo config_text_field("magictouch_ext_exclude","Extensions to exclude <br />(comma separated)",implode(',',$magictouch_ext_exclude));?>   
+<?php echo config_text_field("magictouch_account_id",$lang["magic_touch_key"],$magictouch_account_id);?>
+<?php echo config_custom_select("magictouch_secure",$lang["https"],array("https","http"),$magictouch_secure);?>
+<?php echo config_text_field("magictouch_ext_exclude",$lang["extensions_to_exclude"],implode(',',$magictouch_ext_exclude));?>   
 <?php $rtypes=get_resource_types();
-echo config_custom_select_multi("magictouch_rt_exclude","Resource Types to exclude <br />(highlight to exclude)",$rtypes,$magictouch_rt_exclude);?>
-<?php echo config_text_field("magictouch_view_page_sizes","View page sizes (in order, which sizes to check for the magictouch larger preview)",implode(',',$magictouch_view_page_sizes));?>
-<?php echo config_text_field("magictouch_preview_page_sizes","Preview page sizes (in order, which sizes to check for the magictouch larger preview)",implode(',',$magictouch_preview_page_sizes));?>
+echo config_custom_select_multi("magictouch_rt_exclude",$lang["resource_types_to_exclude"],$rtypes,$magictouch_rt_exclude);?>
+<?php echo config_text_field("magictouch_view_page_sizes",$lang["view_page_sizes"],implode(',',$magictouch_view_page_sizes));?>
+<?php echo config_text_field("magictouch_preview_page_sizes",$lang["preview_page_sizes"],implode(',',$magictouch_preview_page_sizes));?>
 
    
 <div class="Question">  
