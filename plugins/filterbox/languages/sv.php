1ead9ab27b05283d58e016036e0c3e53cb13a8e9
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-02-20 10:13:24 +0000

    Filterbox Plugin: Added a Swedish language file (complete).
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3219 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/filterbox/languages/sv.php
@@ -0,0 +1,10 @@
+<?php
+# Swedish
+# Language File for the Filterbox Plugin
+# Updated by Henrik Frizén 20120220 for svn r3218
+# -------
+#
+#
+$lang['filtertitle']='Filter';
+$lang['filtertext']='Förfina resultatet av aktuell sökning';
+$lang['filterbutton']='Filtrera';
