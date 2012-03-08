f279ad2fc4d906e258eb7fa358feb52baa956035
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-03-08 14:17:06 +0000

    Made the system setup more stylable. The system setup now includes the colour theme css file after muse.css and also includes css files from plugins. Added some css to the default colour themes - to keep the current look.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3237 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/admin/include/header.php
+++ pages/admin/include/header.php
@@ -1,11 +1,10 @@
 <html>
 <head>
-<link href="<?php echo $baseurl?>/css/Col-<?php echo (isset($userfixedtheme) && $userfixedtheme!="")?$userfixedtheme:getval("colourcss",$defaulttheme)?>.css" rel="stylesheet" type="text/css" media="screen,projection,print" id="colourcss" />
 <link rel="stylesheet" type="text/css" href="<?php echo $baseurl?>/pages/admin/muse.css" />
 <script src="../../lib/CodeMirror/js/codemirror.js" type="text/javascript"></script>
 <script type="text/javascript" src="../../lib/js/jquery-1.6.1.min.js"></script>
 <style type="text/css">
-      .CodeMirror-line-numbers {
+    .CodeMirror-line-numbers {
         width: 2.5em;
         color: #aaa;
         background-color: #eee;
@@ -14,12 +13,30 @@
         font-size: 10pt;
         font-family: monospace;
         padding-top: .4em;
-      }
-    </style>
-<style>
-td a {color: black;}
-body {color: black;}
+    }
 </style>
+<link href="<?php echo $baseurl?>/css/Col-<?php echo (isset($userfixedtheme) && $userfixedtheme!="")?$userfixedtheme:getval("colourcss",$defaulttheme)?>.css" rel="stylesheet" type="text/css" media="screen,projection,print" id="colourcss" />
+<?php
+# Include CSS files for for each of the plugins too (if provided)
+for ($n=0;$n<count($plugins);$n++)
+    {
+    $csspath=dirname(__FILE__)."/../../../plugins/" . $plugins[$n] . "/css/style.css";
+    if (file_exists($csspath))
+        {
+        ?>
+        <link href="<?php echo $baseurl?>/plugins/<?php echo $plugins[$n]?>/css/style.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css" media="screen,projection,print"  />
+        <?php
+        }
+    $theme=((isset($userfixedtheme) && $userfixedtheme!=""))?$userfixedtheme:getval("colourcss",$defaulttheme);
+    $csspath=dirname(__FILE__)."/../../../plugins/" . $plugins[$n] . "/css/Col-".$theme.".css";
+    if (file_exists($csspath))
+        {
+        ?>
+        <link href="<?php echo $baseurl?>/plugins/<?php echo $plugins[$n]?>/css/Col-<?php echo $theme?>.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css" media="screen,projection,print" id="<?php echo $plugins[$n]?>css" />
+        <?php
+        }
+    }
+?>
 <title>Administration</title>
 
 </head>
