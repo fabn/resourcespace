ea9c73a86e1692fda9b60609c149625bcc6dae8c
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-03-19 22:39:15 +0000

    update to annotate setup page
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3293 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/annotate/pages/setup.php
+++ plugins/annotate/pages/setup.php
@@ -6,21 +6,24 @@ include "../../../include/general.php";
 if (getval("submit","")!="")
 	{
 	$annotate_ext_exclude=explode(",",getvalescaped("extexclude",""));
+	$annotate_debug=getvalescaped("debug","");
 	if (isset($_POST['rtexclude'])){
 		$annotate_rt_exclude=$_POST['rtexclude'];
 	}
 	else {
 		$annotate_rt_exclude=array();
 	}
-	$annotate_public_view=$_POST['annotate_public_view'];
-	$annotate_show_author=$_POST['annotate_show_author'];	
-
+	$annotate_public_view=getvalescaped("annotate_public_view","");
+	$annotate_show_author=getvalescaped("annotate_show_author","");	
+	$annotate_font=getvalescaped("annotate_font","");
+	
 	$config=array();
 	$config['annotate_rt_exclude']=$annotate_rt_exclude;
 	$config['annotate_ext_exclude']=$annotate_ext_exclude;
+	$config['annotate_debug']=$annotate_debug;
 	$config['annotate_public_view']=$annotate_public_view;
 	$config['annotate_show_author']=$annotate_show_author;
-
+	$config['annotate_font']=$annotate_font;
 	set_plugin_config("annotate",$config);
 	
 	redirect("pages/team/team_home.php");
@@ -38,6 +41,8 @@ include "../../../include/header.php";
 <?php echo config_text_field("extexclude",$lang["extensions_to_exclude"],implode(',',$annotate_ext_exclude));?>   
 <?php $rtypes=get_resource_types();
 echo config_custom_select_multi("rtexclude",$lang["resource_types_to_exclude"],$rtypes,$annotate_rt_exclude);?>
+<?php echo config_custom_select("annotate_font",$lang["annotate_font"],array("helvetica","dejavusanscondensed"),$annotate_font);?>
+<?php echo config_boolean_field("debug",$lang["annotatedebug"],$annotate_debug);?>
 <?php echo config_boolean_field("annotate_public_view",$lang["annotate_public_view"],$annotate_public_view);?>
 <?php echo config_boolean_field("annotate_show_author",$lang["annotate_show_author"],$annotate_show_author);?>
 
