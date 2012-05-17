7d936b90fc7a4825736a157277b941bc6a7c679c
ddwiggins <ddwiggins@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-05-17 14:20:24 +0000

    change default wait time in staticsync_alt from 30 seconds to 2 minutes to attempt to prevent missing companion alternate files
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3384 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/staticsync_alt.php
+++ pages/tools/staticsync_alt.php
@@ -19,7 +19,7 @@ if ($staticsync_ingest){
 $staticsync_alt_suffix_array = array('_alt','_verso','_DNG','_VERSO','_ALT','_dng','_orig','_ORIG','_tp','_TP','_tpv','_TPV','_cov','_COV','_ex','_EX','_scr','_SCR');
 $staticsync_alt_suffixes = true;
 $numeric_alt_suffixes = 8;
-$file_minimum_age = 30; // don't touch files that aren't at least this many seconds old
+$file_minimum_age = 120; // don't touch files that aren't at least this many seconds old
 
 
 if ($numeric_alt_suffixes > 0){
