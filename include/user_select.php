ab1ec60c7702d28dc588a5399cad51d8a3a8c94b
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-07-07 15:21:17 +0000

    use a placeholder instead of a value for starttypingausername. this is supported by most browsers now and it's annoying if that text gets added to the user share
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3508 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/user_select.php
+++ include/user_select.php
@@ -10,7 +10,7 @@ if ($userstring=="") {$userstring=$default_user_select;}
 <table cellpadding="0" cellspacing="0" width="300">
 
 <!-- autocomplete -->
-<tr><td><input type="text" class="medwidth" value="<?php echo $lang["starttypingusername"]?>" id="autocomplete" name="autocomplete_parameter" onClick="this.value='';" /></td>
+<tr><td><input type="text" class="medwidth" placeholder="<?php echo $lang["starttypingusername"]?>" id="autocomplete" name="autocomplete_parameter" onClick="this.value='';" /></td>
 <td><input type=button value="+" class="medcomplementwidth" onClick="addUser();" /></td></tr>
 <!-- -->
 
