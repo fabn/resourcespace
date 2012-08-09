cc469a230ca9741f356c06b500734e07eaff73d1
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-08-09 19:40:52 +0000

    add subject and text as required fields for team_mail.php, as an example of using jQuery Validation
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3605 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/team/team_mail.php
+++ pages/team/team_mail.php
@@ -14,13 +14,22 @@ if (getval("send","")!="")
 	$result=bulk_mail(getvalescaped("users",""),getvalescaped("subject",""),getvalescaped("text",""),getval("html","")=="yes");
 	if ($result=="") {$error=$lang["emailsent"];} else {$error="!! " . $result . " !!";}
 	}
+$headerinsert.="
+<script src=\"$baseurl/lib/js/jquery.validate.min.js\" type=\"text/javascript\"></script><script type=\"text/javascript\">
+jQuery(document).ready(function(){
+	jQuery('#myform').validate({ 
+		errorPlacement: function(error, element) {
+		element.after('<span class=\"FormError\">'+error.html()+'</span>');
+		},
+   wrapper: 'div'});
+});
+</script>";
 
 include "../../include/header.php";
 ?>
 <div class="BasicsBox">
 <h1><?php echo $lang["sendbulkmail"]?></h1>
-
-<form method="post" action="team_mail.php">
+<form id="myform" method="post" action="team_mail.php">
 
 <?php if (isset($error)) { ?><div class="FormError"><?php echo $error?></div><?php } ?>
 
@@ -29,9 +38,9 @@ include "../../include/header.php";
 
 <div class="Question"><label><?php echo $lang["emailhtml"]?></label><input name="html" type="checkbox" value="yes" <?php if (getval("html","")=="yes") { ?>checked<?php } ?>><div class="clearerleft"> </div></div>
 
-<div class="Question"><label><?php echo $lang["emailsubject"]?></label><input name="subject" type="text" class="stdwidth" value="<?php echo getval("subject",$applicationname)?>"><div class="clearerleft"> </div></div>
+<div class="Question"><label><?php echo $lang["emailsubject"]?></label><input name="subject" type="text" class="stdwidth Inline required" value="<?php echo getval("subject",$applicationname)?>"><div class="clearerleft"> </div></div>
 
-<div class="Question"><label><?php echo $lang["emailtext"]?></label><textarea name="text" class="stdwidth" rows=25 cols=50><?php echo htmlspecialchars(getval("text",""))?></textarea><div class="clearerleft"> </div></div>
+<div class="Question"><label><?php echo $lang["emailtext"]?></label><textarea name="text" class="stdwidth Inline required" rows=25 cols=50><?php echo htmlspecialchars(getval("text",""))?></textarea><div class="clearerleft"> </div></div>
 
 <?php hook("additionalemailfield");?>
 
