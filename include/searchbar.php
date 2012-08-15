69208c061337210f69e779a5ec46ae0a8cfa3a00
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-08-15 13:03:47 +0000

    New hook to allow advanced search link to be replaced / removed
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3625 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/searchbar.php
+++ include/searchbar.php
@@ -528,7 +528,7 @@ if (!$basic_simple_search)
   <br />
   <?php hook("searchbarbeforebottomlinks"); ?>
   <?php if (! $disable_geocoding) { ?><p><a href="<?php echo $baseurl?>/pages/geo_search.php">&gt; <?php echo $lang["geographicsearch"]?></a></p><?php } ?>
-  <?php if (! $advancedsearch_disabled) { ?><p><a href="<?php echo $baseurl?>/pages/search_advanced.php">&gt; <?php echo $lang["gotoadvancedsearch"]?></a></p><?php } ?>
+  <?php if (! $advancedsearch_disabled && !hook("advancedsearchlink")) { ?><p><a href="<?php echo $baseurl?>/pages/search_advanced.php">&gt; <?php echo $lang["gotoadvancedsearch"]?></a></p><?php } ?>
 
   <?php hook("searchbarafterbuttons"); ?>
 
