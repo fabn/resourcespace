a90a392d09cffe027a0f117535646c26be1d80f7
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-08-15 11:22:46 +0000

    Further fixs for resourceconnect
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3621 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/resourceconnect/hooks/preview.php
+++ plugins/resourceconnect/hooks/preview.php
@@ -14,3 +14,34 @@ function HookResourceconnectPreviewBeforefooter()
 	<!-- END GRAB -->
 	<?php
 	}
+
+function HookResourceconnectPreviewNextpreviousextraurl()
+	{
+	if (getval("resourceconnect_source","")=="") {return false;} # Not a ResourceConnect result set. 
+
+	# Forward the resourceconnect source.
+
+	global $baseurl;
+	echo "resourceconnect_source=" .$baseurl;
+	}
+	
+function HookResourceconnectPreviewViewextraurl()
+	{
+	if (getval("resourceconnect_source","")=="") {return false;} # Not a ResourceConnect result set. 
+
+	# Forward the resourceconnect source.
+
+	global $baseurl;
+	echo "resourceconnect_source=" .$baseurl;
+	}
+	
+function HookResourceconnectPreviewSearchextraurl()
+	{
+	if (getval("resourceconnect_source","")=="") {return false;} # Not a ResourceConnect result set. 
+
+	# Forward the resourceconnect source.
+
+	global $baseurl;
+	echo "resourceconnect_source=" .$baseurl;
+	}
+	
