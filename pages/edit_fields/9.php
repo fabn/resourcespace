537b89df4461c547e4bf0e006b7f803301191c30
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-08-16 20:12:32 +0000

    another jQuery update
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3632 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/9.php
+++ pages/edit_fields/9.php
@@ -14,116 +14,117 @@ $readonly=($pagename=="search_advanced");
 
 <script type="text/javascript">
 
-jQuery('#<?php echo $name?>_selector').autocomplete( { source: "<?php echo $baseurl?>/pages/edit_fields/9_ajax/suggest_keywords.php?field=<?php echo $field["ref"] ?>&readonly=<?php echo $readonly ?>", 
-	select : selectKeyword_<?php echo $name ?>
-	});
+jQuery(document).ready(function () {
+	jQuery('#<?php echo $name?>_selector').autocomplete( { source: "<?php echo $baseurl?>/pages/edit_fields/9_ajax/suggest_keywords.php?field=<?php echo $field["ref"] ?>&readonly=<?php echo $readonly ?>", 
+		select : selectKeyword_<?php echo $name ?>
+		});
 
-var Keywords_<?php echo $name ?>= new Array();
-var KeywordCounter_<?php echo $name ?>=0;
-var KeywordsTranslated_<?php echo $name ?>= new Array();
+	var Keywords_<?php echo $name ?>= new Array();
+	var KeywordCounter_<?php echo $name ?>=0;
+	var KeywordsTranslated_<?php echo $name ?>= new Array();
 
-function selectKeyword_<?php echo $name ?>(event, ui)
-	{
-	// var keyword=document.getElementById("<?php echo $name ?>_selector").value;
-	var keyword=ui.item.value;
-	
-	if (keyword.substring(0,<?php echo strlen($lang["createnewentryfor"]) ?>)=="<?php echo $lang["createnewentryfor"] ?>")
+	function selectKeyword_<?php echo $name ?>(event, ui)
 		{
-		keyword=keyword.substring(<?php echo strlen($lang["createnewentryfor"])+1 ?>);
+		// var keyword=document.getElementById("<?php echo $name ?>_selector").value;
+		var keyword=ui.item.value;
+		
+		if (keyword.substring(0,<?php echo strlen($lang["createnewentryfor"]) ?>)=="<?php echo $lang["createnewentryfor"] ?>")
+			{
+			keyword=keyword.substring(<?php echo strlen($lang["createnewentryfor"])+1 ?>);
 
-		// Add the word.
-		jQuery.post("<?php echo $baseurl?>/pages/edit_fields/9_ajax/add_keyword.php?field=<?php echo $field["ref"] ?>&keyword=" + encodeURI(keyword));
+			// Add the word.
+			jQuery.post("<?php echo $baseurl?>/pages/edit_fields/9_ajax/add_keyword.php?field=<?php echo $field["ref"] ?>&keyword=" + encodeURI(keyword));
+			}
+
+		addKeyword_<?php echo $name ?>(keyword);
+		updateSelectedKeywords_<?php echo $name ?>(true);
+		document.getElementById('<?php echo $name ?>_selector').value='';
+		return false;
 		}
 
-	addKeyword_<?php echo $name ?>(keyword);
-	updateSelectedKeywords_<?php echo $name ?>(true);
-	document.getElementById('<?php echo $name ?>_selector').value='';
-	return false;
-	}
-
-function addKeyword_<?php echo $name ?>(keyword)
-	{
-	removeKeyword_<?php echo $name ?>(keyword,false); // remove any existing match in the list.
-	Keywords_<?php echo $name ?>[KeywordCounter_<?php echo $name ?>]=keyword;
-	KeywordCounter_<?php echo $name ?>++;
-	}
-
-function removeKeyword_<?php echo $name ?>(keyword,user_action)
-	{
-	var replacement=Keywords_<?php echo $name ?>;
-	counter=0;
-	for (var n=0;n<KeywordCounter_<?php echo $name ?>;n++)
+	function addKeyword_<?php echo $name ?>(keyword)
 		{
-		if (keyword!=Keywords_<?php echo $name ?>[n]) {replacement[counter]=Keywords_<?php echo $name ?>[n];counter++;}
+		removeKeyword_<?php echo $name ?>(keyword,false); // remove any existing match in the list.
+		Keywords_<?php echo $name ?>[KeywordCounter_<?php echo $name ?>]=keyword;
+		KeywordCounter_<?php echo $name ?>++;
 		}
-	Keywords_<?php echo $name ?> = replacement;
-	KeywordCounter_<?php echo $name ?> =counter;
-	updateSelectedKeywords_<?php echo $name ?>(user_action);
-	}
 
-function updateSelectedKeywords_<?php echo $name ?>(user_action)
-	{
-	var html="";
-	var value="";
-	for (var n=0;n<KeywordCounter_<?php echo $name ?>;n++)
+	function removeKeyword_<?php echo $name ?>(keyword,user_action)
 		{
-		html+='<a href="#" onClick="removeKeyword_<?php echo $name ?>(\'' + Keywords_<?php echo $name ?>[n] +'\',true);return false;">[ x ]</a> &nbsp;' + Keywords_<?php echo $name ?>[n] + '<br/>';
-		value+="," + resolveTranslated_<?php echo $name ?>(Keywords_<?php echo $name ?>[n]);
+		var replacement=Keywords_<?php echo $name ?>;
+		counter=0;
+		for (var n=0;n<KeywordCounter_<?php echo $name ?>;n++)
+			{
+			if (keyword!=Keywords_<?php echo $name ?>[n]) {replacement[counter]=Keywords_<?php echo $name ?>[n];counter++;}
+			}
+		Keywords_<?php echo $name ?> = replacement;
+		KeywordCounter_<?php echo $name ?> =counter;
+		updateSelectedKeywords_<?php echo $name ?>(user_action);
 		}
-	document.getElementById('<?php echo $name?>_selected').innerHTML=html;
-	document.getElementById('<?php echo $name?>').value=value;
-	
-	// Update the result counter, if the function is available (e.g. on Advanced Search).
-	if( typeof( UpdateResultCount ) == 'function' )
-		{
-		UpdateResultCount();
-		}
-	<?php if ($edit_autosave) {?>if (user_action) {AutoSave('<?php echo $fields[$n]["ref"] ?>');}<?php } ?>
-	}
-	
-function resolveTranslated_<?php echo $name ?>(keyword)
-	{
-	if (typeof KeywordsTranslated_<?php echo $name ?>[keyword]=='undefined')
-		{
-		return keyword;
-		}
-	else
-		{
-		return KeywordsTranslated_<?php echo $name ?>[keyword];
-		}
-	}
 
-<?php 
-# Load translations - store original untranslated strings for each keyword, as this is what is actually set.
-$options=trim_array(explode(",",$field["options"]));
-for ($m=0;$m<count($options);$m++)
-	{
-	$trans=i18n_get_translated($options[$m]);
-	if ($trans!="" && $trans!=$options[$m]) # Only add if actually different (i.e., an i18n string)
+	function updateSelectedKeywords_<?php echo $name ?>(user_action)
 		{
-		?>
-		KeywordsTranslated_<?php echo $name ?>["<?php echo $trans ?>"]="<?php echo $options[$m] ?>";
-		<?php
+		var html="";
+		var value="";
+		for (var n=0;n<KeywordCounter_<?php echo $name ?>;n++)
+			{
+			html+='<a href="#" onClick="removeKeyword_<?php echo $name ?>(\'' + Keywords_<?php echo $name ?>[n] +'\',true);return false;">[ x ]</a> &nbsp;' + Keywords_<?php echo $name ?>[n] + '<br/>';
+			value+="," + resolveTranslated_<?php echo $name ?>(Keywords_<?php echo $name ?>[n]);
+			}
+		document.getElementById('<?php echo $name?>_selected').innerHTML=html;
+		document.getElementById('<?php echo $name?>').value=value;
+		
+		// Update the result counter, if the function is available (e.g. on Advanced Search).
+		if( typeof( UpdateResultCount ) == 'function' )
+			{
+			UpdateResultCount();
+			}
+		<?php if ($edit_autosave) {?>if (user_action) {AutoSave('<?php echo $fields[$n]["ref"] ?>');}<?php } ?>
 		}
-	}
-
-
-# Select all selected options
-$options=trim_array(explode(",",$value));
-for ($m=0;$m<count($options);$m++)
-	{
-	$trans=i18n_get_translated($options[$m]);
-	if ($trans!="")
+		
+	function resolveTranslated_<?php echo $name ?>(keyword)
 		{
-		?>
-		addKeyword_<?php echo $name ?>("<?php echo $trans ?>");
-		<?php
+		if (typeof KeywordsTranslated_<?php echo $name ?>[keyword]=='undefined')
+			{
+			return keyword;
+			}
+		else
+			{
+			return KeywordsTranslated_<?php echo $name ?>[keyword];
+			}
 		}
-	}
-?>
-updateSelectedKeywords_<?php echo $name ?>(false);
+
+	<?php 
+	# Load translations - store original untranslated strings for each keyword, as this is what is actually set.
+	$options=trim_array(explode(",",$field["options"]));
+	for ($m=0;$m<count($options);$m++)
+		{
+		$trans=i18n_get_translated($options[$m]);
+		if ($trans!="" && $trans!=$options[$m]) # Only add if actually different (i.e., an i18n string)
+			{
+			?>
+			KeywordsTranslated_<?php echo $name ?>["<?php echo $trans ?>"]="<?php echo $options[$m] ?>";
+			<?php
+			}
+		}
 
 
+	# Select all selected options
+	$options=trim_array(explode(",",$value));
+	for ($m=0;$m<count($options);$m++)
+		{
+		$trans=i18n_get_translated($options[$m]);
+		if ($trans!="")
+			{
+			?>
+			addKeyword_<?php echo $name ?>("<?php echo $trans ?>");
+			<?php
+			}
+		}
+	?>
+	updateSelectedKeywords_<?php echo $name ?>(false);
+
+})
 </script>
 <?php
 /* include dirname(__FILE__) . "/../../include/user_select.php"; 
