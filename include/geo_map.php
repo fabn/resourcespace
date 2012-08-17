f3e87c6371a501ef9f4e03ce38e56f3b6b07173f
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-08-17 18:03:48 +0000

    geo_map is blocked for https installations in chrome unless the google url is https, so I think we need an https url here which will work in both cases
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3635 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/geo_map.php
+++ include/geo_map.php
@@ -3,7 +3,7 @@
 <div id="map_canvas" style="width: *; height: <?php echo isset($mapheight)?$mapheight:"500" ?>px; display:block; float:none;" class="Picture" ></div>
 
 <script src="../lib/OpenLayers/OpenLayers.js"></script>
-<script src="http://maps.google.com/maps/api/js?v=3.2&sensor=false"></script>
+<script src="https://maps.google.com/maps/api/js?v=3.2&sensor=false"></script>
 <script>
 OpenLayers.Lang.setCode("<?php echo $language?>");
 map = new OpenLayers.Map("map_canvas");
