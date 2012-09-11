1eb996950227f40231c571d814e1fad7734913f2
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-09-11 10:45:38 +0000

    Minor change to location map - JS load uses complete URL so it works within a plugin page (necessary for ResourceConnect)
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3685 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/geo_map.php
+++ include/geo_map.php
@@ -2,7 +2,7 @@
 
 <div id="map_canvas" style="width: *; height: <?php echo isset($mapheight)?$mapheight:"500" ?>px; display:block; float:none;" class="Picture" ></div>
 
-<script src="../lib/OpenLayers/OpenLayers.js"></script>
+<script src="<?php echo $baseurl ?>/lib/OpenLayers/OpenLayers.js"></script>
 <script src="https://maps.google.com/maps/api/js?v=3.2&sensor=false"></script>
 <script>
 OpenLayers.Lang.setCode("<?php echo $language?>");
